6f4cd50c9d83ff32ee4d1a0729f06dcb156b1f0f
HEYAHONG <2229388563@qq.com>
2024-09-16 19:54:15 +0800

    完善套接字接口

--- hcppbox/HCPPSocket.h
+++ hcppbox/HCPPSocket.h
@@ -87,6 +87,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 
 /*
@@ -101,6 +103,176 @@
  */
 #ifndef INVALID_SOCKET
 #define INVALID_SOCKET (-1)
+#endif
+
+/*
+ *  套接字地址,此地址一般不直接使用，一般由具体的套接字地址(HCPPSocketAddress*)指针转化为本类型的指针（其作用类似void*）调用API（如bind）
+ */
+typedef struct sockaddr HCPPSocketAddress;
+/*
+ *  套接字地址长度
+ */
+typedef socklen_t HCPPSocketAddressLength;
+/*
+ *  地址大小默认为16字节
+ */
+#define HCPPSOCKETADDRESS_MAX_LENGTH sizeof(HCPPSocketAddress)
+
+#ifdef HCPPSOCKET_HAVE_SOCKET_IPV4
+/*
+ *  IPV4套接字地址,可转化为HCPPSocketAddress*;
+ */
+typedef struct sockaddr_in HCPPSocketAddressIPV4;
+
+/*
+ *  地址大小默认为16字节
+ */
+#undef  HCPPSOCKETADDRESS_MAX_LENGTH
+#define HCPPSOCKETADDRESS_MAX_LENGTH (sizeof(HCPPSocketAddressIPV4))
+
+#endif
+
+#ifdef HCPPSOCKET_HAVE_SOCKET_IPV6
+/*
+ *  IPV6套接字地址,可转化为HCPPSocketAddress*;
+ */
+typedef struct sockaddr_in6 HCPPSocketAddressIPV6;
+
+/*
+ *  地址大小默认为28字节
+ */
+#undef  HCPPSOCKETADDRESS_MAX_LENGTH
+#define HCPPSOCKETADDRESS_MAX_LENGTH (sizeof(HCPPSocketAddressIPV6))
+
+#endif
+
+#ifdef HCPPSOCKET_HAVE_SOCKET_UNIX
+/*
+ *  UNIX套接字(文件)地址,可转化为HCPPSocketAddress*;
+ */
+typedef struct sockaddr_un HCPPSocketAddressUNIX;
+
+/*
+ *  地址大小默认为110字节
+ */
+#undef  HCPPSOCKETADDRESS_MAX_LENGTH
+#define HCPPSOCKETADDRESS_MAX_LENGTH (sizeof(HCPPSocketAddressUNIX))
+
+#endif
+
+#ifdef __cplusplus
+
+/*
+ * 自动套接字地址，可用于通用API编写，但可能浪费部分内存空间（会使用占用空间最大的套接字地址长度）
+ */
+class HCPPSocketAddressAuto
+{
+    struct
+    {
+        int8_t pad[HCPPSOCKETADDRESS_MAX_LENGTH];
+    }   addr;
+public:
+    HCPPSocketAddressAuto()
+    {
+        memset(&addr,0,sizeof(addr));
+    }
+    HCPPSocketAddressAuto(HCPPSocketAddressAuto &oths):addr(oths.addr)
+    {
+
+    }
+    HCPPSocketAddressAuto(HCPPSocketAddressAuto &&oths):addr(oths.addr)
+    {
+
+    }
+    virtual ~HCPPSocketAddressAuto()
+    {
+
+    }
+    operator HCPPSocketAddress *()
+    {
+        return (HCPPSocketAddress *)(void *)addr.pad;
+    }
+#ifdef  HCPPSOCKET_HAVE_SOCKET_IPV4
+    operator HCPPSocketAddressIPV4 *()
+    {
+        if(((HCPPSocketAddress *)(*this))->sa_family==AF_INET)
+        {
+            return (HCPPSocketAddressIPV4 *)(void *)addr.pad;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+#endif
+
+#ifdef  HCPPSOCKET_HAVE_SOCKET_IPV6
+    operator HCPPSocketAddressIPV6 *()
+    {
+        if(((HCPPSocketAddress *)(*this))->sa_family==AF_INET6)
+        {
+            return (HCPPSocketAddressIPV6 *)(void *)addr.pad;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+#endif
+
+#ifdef  HCPPSOCKET_HAVE_SOCKET_UNIX
+    operator HCPPSocketAddressUNIX *()
+    {
+        if(((HCPPSocketAddress *)(*this))->sa_family==AF_UNIX)
+        {
+            return (HCPPSocketAddressUNIX *)(void *)addr.pad;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+#endif
+
+    HCPPSocketAddressLength Length()
+    {
+        HCPPSocketAddressLength len=sizeof(addr);
+        switch(((HCPPSocketAddress *)(*this))->sa_family)
+        {
+#ifdef  HCPPSOCKET_HAVE_SOCKET_IPV4
+        case AF_INET:
+        {
+            len=sizeof(HCPPSocketAddressIPV4);
+        }
+        break;
+#endif
+#ifdef  HCPPSOCKET_HAVE_SOCKET_IPV6
+        case AF_INET6:
+        {
+            len=sizeof(HCPPSocketAddressIPV6);
+        }
+        break;
+#endif
+#ifdef  HCPPSOCKET_HAVE_SOCKET_UNIX
+        case AF_UNIX:
+        {
+            len=sizeof(HCPPSocketAddressUNIX);
+        }
+        break;
+#endif
+        default:
+            break;
+        }
+        return len;
+    }
+
+    operator HCPPSocketAddressLength()
+    {
+        return Length();
+    }
+
+};
+
 #endif
 
 #ifdef __cplusplus
