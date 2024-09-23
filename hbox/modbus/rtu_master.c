234d4cd3859273c8588f59bde9e0d6d321790c97
HEYAHONG <2229388563@qq.com>
2024-09-23 16:58:43 +0800

    优化代码

--- hbox/modbus/rtu_master.c
+++ hbox/modbus/rtu_master.c
@@ -2,7 +2,7 @@
 
 bool modbus_rtu_master_request(modbus_io_interface_t *io,uint8_t function_code,void *context,size_t context_length)
 {
-    return modbus_io_interface_request(MODBUS_IO_INTERFACE_RTU_MASTER,io,function_code,context,context_length);
+    return modbus_io_interface_request(MODBUS_IO_INTERFACE_RTU_MASTER,(modbus_io_interface_t *)io,function_code,context,context_length);
 }
 
 
--- hbox/modbus/tcp_client.c
+++ hbox/modbus/tcp_client.c
@@ -3,13 +3,13 @@
 
 bool modbus_tcp_client_request(modbus_tcp_client_io_interface_t *io,uint8_t function_code,void *context,size_t context_length)
 {
-    return modbus_io_interface_request(MODBUS_IO_INTERFACE_TCP_CLIENT,io,function_code,context,context_length);
+    return modbus_io_interface_request(MODBUS_IO_INTERFACE_TCP_CLIENT,(modbus_io_interface_t *)io,function_code,context,context_length);
 }
 
 
 bool modbus_tcp_client_request_gateway(modbus_tcp_client_io_interface_t *io,uint8_t function_code,void *context,size_t context_length)
 {
-    return modbus_io_interface_request(MODBUS_IO_INTERFACE_TCP_GATEWAY_CLIENT,io,function_code,context,context_length);
+    return modbus_io_interface_request(MODBUS_IO_INTERFACE_TCP_GATEWAY_CLIENT,(modbus_io_interface_t *)io,function_code,context,context_length);
 }
 
 
