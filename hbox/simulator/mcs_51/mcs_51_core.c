e1e7cf6f2b1203027121b343cf3bd865a89557ed
HEYAHONG <2229388563@qq.com>
2024-11-21 14:30:15 +0800

    初步完成MCS-51中断设置功能

--- hbox/simulator/mcs_51/mcs_51_core.c
+++ hbox/simulator/mcs_51/mcs_51_core.c
@@ -14,6 +14,10 @@ struct hs_mcs_51_core
     struct
     {
         uint32_t delay_tick:2;//MCS-51具有多周期指令。为保证执行效果，对于多周期指令需要延时,最多延时3周期。
+        uint32_t interrupt_nested:2;//中断嵌套层数，0=正常运行
+        uint32_t pc:16;//PC
+        uint32_t interrupt_low_priority_scan_table;//中断(低优先级)扫描表，位0表示中断0，最高支持32个中断
+        uint32_t interrupt_high_priority_scan_table;//中断(高优先级)扫描表，位0表示中断0，最高支持32个中断
     };
 };
 
@@ -35,6 +39,51 @@ hs_mcs_51_core_t *hs_mcs_51_core_init(void *mem,hs_mcs_51_io_t io,void *usr)
     return NULL;
 }
 
+static void hs_mcs_51_core_scan_interrupt(hs_mcs_51_core_t * core)
+{
+    if(core != NULL)
+    {
+        if(core->interrupt_nested < 2)
+        {
+            if(core->interrupt_high_priority_scan_table!=0)
+            {
+                for(uint8_t i=0; i<32; i++)
+                {
+                    if((core->interrupt_high_priority_scan_table&(1ULL<<i))!=0)
+                    {
+                        core->interrupt_high_priority_scan_table &= (~(1ULL<<i));
+                        uint16_t address=3+8*i;
+                        core->interrupt_nested++;//增加中断嵌套，RETI指令时自减1
+                        {
+                            //TODO:保存现场,执行中断
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+        if(core->interrupt_nested == 0)
+        {
+            if(core->interrupt_low_priority_scan_table!=0)
+            {
+                for(uint8_t i=0; i<32; i++)
+                {
+                    if((core->interrupt_low_priority_scan_table&(1ULL<<i))!=0)
+                    {
+                        core->interrupt_low_priority_scan_table &= (~(1ULL<<i));
+                        uint16_t address=3+8*i;
+                        core->interrupt_nested++;//增加中断嵌套，RETI指令时自减1
+                        {
+                            //TODO:保存现场,执行中断
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+
 void hs_mcs_51_core_tick(hs_mcs_51_core_t * core,size_t cycles)
 {
     if(core!=NULL)
@@ -46,6 +95,8 @@ void hs_mcs_51_core_tick(hs_mcs_51_core_t * core,size_t cycles)
                 core->delay_tick--;
                 continue;
             }
+
+            hs_mcs_51_core_scan_interrupt(core);
 
             //TODO:执行指令
         }
@@ -65,6 +116,21 @@ void hs_mcs_51_core_reset(hs_mcs_51_core_t * core)
         {
             uint8_t dummy=0;
             core->io(core,HS_MCS_51_IO_RESET,0,&dummy,sizeof(dummy),core->usr);
+        }
+    }
+}
+
+void hs_mcs_51_core_interrupt_set(hs_mcs_51_core_t * core,hs_mcs_51_interrupt_number_t number,bool is_high_priority)
+{
+    if(core!=NULL)
+    {
+        if(is_high_priority)
+        {
+            core->interrupt_high_priority_scan_table|=(1ULL << ((size_t)number));
+        }
+        else
+        {
+            core->interrupt_low_priority_scan_table|=(1ULL << ((size_t)number));
         }
     }
 }
--- hbox/simulator/mcs_51/mcs_51_core.h
+++ hbox/simulator/mcs_51/mcs_51_core.h
@@ -82,6 +82,57 @@ void hs_mcs_51_core_tick(hs_mcs_51_core_t * core,size_t cycles);
  */
 void hs_mcs_51_core_reset(hs_mcs_51_core_t * core);
 
+//中断表（中断入口地址=(中断号 *8)+3）
+typedef enum
+{
+    HS_MCS_51_INTERRUPT_0=0,
+    HS_MCS_51_INTERRUPT_1=1,
+    HS_MCS_51_INTERRUPT_2=2,
+    HS_MCS_51_INTERRUPT_3=3,
+    HS_MCS_51_INTERRUPT_4=4,
+    HS_MCS_51_INTERRUPT_5=5,
+    HS_MCS_51_INTERRUPT_6=6,
+    HS_MCS_51_INTERRUPT_7=7,
+    HS_MCS_51_INTERRUPT_8=8,
+    HS_MCS_51_INTERRUPT_9=9,
+    HS_MCS_51_INTERRUPT_10=10,
+    HS_MCS_51_INTERRUPT_11=11,
+    HS_MCS_51_INTERRUPT_12=12,
+    HS_MCS_51_INTERRUPT_13=13,
+    HS_MCS_51_INTERRUPT_14=14,
+    HS_MCS_51_INTERRUPT_15=15,
+    HS_MCS_51_INTERRUPT_16=16,
+    HS_MCS_51_INTERRUPT_17=17,
+    HS_MCS_51_INTERRUPT_18=18,
+    HS_MCS_51_INTERRUPT_19=19,
+    HS_MCS_51_INTERRUPT_20=20,
+    HS_MCS_51_INTERRUPT_21=21,
+    HS_MCS_51_INTERRUPT_22=22,
+    HS_MCS_51_INTERRUPT_23=23,
+    HS_MCS_51_INTERRUPT_24=24,
+    HS_MCS_51_INTERRUPT_25=25,
+    HS_MCS_51_INTERRUPT_26=26,
+    HS_MCS_51_INTERRUPT_27=27,
+    HS_MCS_51_INTERRUPT_28=28,
+    HS_MCS_51_INTERRUPT_29=29,
+    HS_MCS_51_INTERRUPT_30=30,
+    HS_MCS_51_INTERRUPT_31=31,
+    HS_MCS_51_INTERRUPT_8051_IE0=HS_MCS_51_INTERRUPT_0, //外部中断0
+    HS_MCS_51_INTERRUPT_8051_TF0=HS_MCS_51_INTERRUPT_1, //定时器中断0
+    HS_MCS_51_INTERRUPT_8051_IE1=HS_MCS_51_INTERRUPT_2, //外部中断1
+    HS_MCS_51_INTERRUPT_8051_TF1=HS_MCS_51_INTERRUPT_3, //定时器中断1
+    HS_MCS_51_INTERRUPT_8051_SI0=HS_MCS_51_INTERRUPT_4, //串口中断0
+} hs_mcs_51_interrupt_number_t;
+
+/** \brief MCS-51内核设置中断到中断扫描表（将在下一个周期执行中断），执行后自动清除，一般由外设调用，注意：此函数不是线程安全的，必要时需要加锁。
+ *
+ * \param core hs_mcs_51_core_t* MCS-51 内核指针
+ * \param number hs_mcs_51_interrupt_number_t 中断号
+ * \param is_high_priority bool 是否是高优先级中断
+ *
+ */
+void hs_mcs_51_core_interrupt_set(hs_mcs_51_core_t * core,hs_mcs_51_interrupt_number_t number,bool is_high_priority);
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
