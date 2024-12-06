c6636d0a14a7c558c4ed8c4cb913a65cd2b5cd53
HEYAHONG <2229388563@qq.com>
2024-12-06 10:35:02 +0800

    完善代码

--- hbox/gui/hgui_gui_common.c
+++ hbox/gui/hgui_gui_common.c
@@ -7,5 +7,58 @@
  * License:   MIT
  **************************************************************/
 #include "hgui_gui_common.h"
+#include "hdefaults.h"
 
+static hgui_gui_malloc_t malloc_hook=NULL;
+void * hgui_gui_malloc(size_t nBytes)
+{
+    if(malloc_hook!=NULL)
+    {
+        return malloc_hook(nBytes);
+    }
+    else
+    {
+        return hgui_gui_default_malloc(nBytes);
+    }
+}
 
+void hgui_gui_malloc_set(hgui_gui_malloc_t hook)
+{
+    malloc_hook=hook;
+}
+
+void * hgui_gui_default_malloc(size_t nBytes)
+{
+    if(nBytes==0)
+    {
+        return NULL;
+    }
+    return hdefaults_malloc(nBytes,NULL);
+}
+
+hgui_gui_free_t free_hook=NULL;
+void hgui_gui_free(void *ptr)
+{
+    if(free_hook!=NULL)
+    {
+        free_hook(ptr);
+    }
+    else
+    {
+        hgui_gui_default_free(ptr);
+    }
+}
+
+void hgui_gui_free_set(hgui_gui_free_t hook)
+{
+    free_hook=hook;
+}
+
+void hgui_gui_default_free(void *ptr)
+{
+    if(ptr==NULL)
+    {
+        return;
+    }
+    hdefaults_free(ptr,NULL);
+}
--- hbox/gui/hgui_gui_common.h
+++ hbox/gui/hgui_gui_common.h
@@ -31,6 +31,60 @@ typedef SSIZE_T ssize_t;
 typedef intptr_t ssize_t;
 #endif // ssize_t
 
+/** \brief GUI内存分配
+ *
+ * \param nBytes size_t 大小
+ * \return void* 指针，失败为NULL
+ *
+ */
+void * hgui_gui_malloc(size_t nBytes);
+
+
+typedef void * (*hgui_gui_malloc_t)(size_t nBytes);
+
+/** \brief 设置GUI内存分配钩子
+ *
+ * \param hook hgui_gui_malloc_t malloc 钩子
+ *
+ */
+void hgui_gui_malloc_set(hgui_gui_malloc_t hook);
+
+
+/** \brief GUI内存默认分配(不受钩子影响)
+ *
+ * \param nBytes size_t 大小
+ * \return void* 指针，失败为NULL
+ *
+ */
+void * hgui_gui_default_malloc(size_t nBytes);
+
+
+/** \brief GUI内存释放
+ *
+ * \param ptr void* 指针
+ *
+ */
+void hgui_gui_free(void *ptr);
+
+
+typedef void (*hgui_gui_free_t)(void *ptr);
+
+/** \brief  设置GUI内存释放钩子
+ *
+ * \param hook hgui_gui_free_t free钩子
+ *
+ */
+void hgui_gui_free_set(hgui_gui_free_t hook);
+
+/** \brief GUI内存默认释放(不受钩子影响)
+ *
+ * \param ptr void* 指针
+ *
+ */
+void hgui_gui_default_free(void *ptr);
+
+
+
 #endif
 
 #ifdef __cplusplus
