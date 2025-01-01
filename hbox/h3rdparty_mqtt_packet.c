7b48f98d5092baf59ca170079b7aac256f1f51d3
HEYAHONG <2229388563@qq.com>
2025-01-01 18:29:19 +0800

    优化部分paho.mqtt.embedded-c函数名

--- hbox/h3rdparty_mqtt_packet.c
+++ hbox/h3rdparty_mqtt_packet.c
@@ -10,6 +10,18 @@
 #include "hdefaults.h"
 
 #ifndef H3RDPARTY_USING_SYSTEM_PAHO_MQTT_EMBEDDED_C_PACKET
+//修改部分容易与其它库冲突的函数名
+#define bufchar             MQTTPacket_bufchar
+#define readInt             MQTTPacket_readInt
+#define readChar            MQTTPacket_readChar
+#define writeChar           MQTTPacket_writeChar
+#define writeInt            MQTTPacket_writeInt
+#define writeCString        MQTTPacket_writeCString
+#define getLenStringLen     MQTTPacket_getLenStringLen
+#define writeMQTTString     MQTTPacket_writeMQTTString
+#define readMQTTLenString   MQTTPacket_readMQTTLenString
+#include H3RDPARTY_PAHO_MQTT_EMBEDDED_C_PACKET_HEADER
+
 #include "3rdparty/paho.mqtt.embedded-c/MQTTPacket/src/MQTTConnectClient.c"
 #ifdef min
 #undef min
