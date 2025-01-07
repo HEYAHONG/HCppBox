7bff37a094002997732483a3199ae92ab6ecc646
HEYAHONG <2229388563@qq.com>
2025-01-07 17:26:19 +0800

    显示默认节拍

--- test/Android/HelloWorld/app/src/main/cpp/native-lib.cpp
+++ test/Android/HelloWorld/app/src/main/cpp/native-lib.cpp
@@ -39,13 +39,13 @@ Java_cn_hyhsystem_hcppbox_helloworld_MainActivity_Init(JNIEnv *env, jobject thiz
 /*
  * 注意：毫秒定时器由定时器(在主活动实现)调用
  */
-hdefaults_tick_t current_tick=0;
+static hdefaults_tick_t current_tick=0;
 extern "C"
 JNIEXPORT void JNICALL
 Java_cn_hyhsystem_hcppbox_helloworld_MainActivity_MsTick(JNIEnv *env, jobject thiz) {
     current_tick++;
     if(current_tick%5000==0)
     {
-        LOGI("Current Tick=%llu",(unsigned  long long)current_tick);
+        LOGI("Current Tick=%llu，SysTick=%llu",(unsigned  long long)current_tick,(unsigned  long long)hdefaults_tick_get());
     }
 }
