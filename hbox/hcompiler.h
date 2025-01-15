23faf5d27ca1757d1c25950a70f57f5fcb4bc7dc
HEYAHONG <2229388563@qq.com>
2025-01-15 14:28:37 +0800

    将Keil常用的编译器的宏定义移动至专用文件

--- hbox/hcompiler.h
+++ hbox/hcompiler.h
@@ -57,63 +57,7 @@
 #endif
 #endif
 
-#ifdef  __CC_ARM
-/*
-Arm Compiler 4/5 (keil mdk)
-*/
-#ifndef   __ASM
-#define __ASM                                  __asm
-#endif
 
-#ifndef   __INLINE
-#define __INLINE                               __inline
-#endif
-
-#ifndef   __STATIC_INLINE
-#define __STATIC_INLINE                        static __inline
-#endif
-
-#ifndef   __STATIC_FORCEINLINE
-#define __STATIC_FORCEINLINE                   static __forceinline
-#endif
-
-#ifndef   __NO_RETURN
-#define __NO_RETURN                            __declspec(noreturn)
-#endif
-
-#ifndef   __USED
-#define __USED                                 __attribute__((used))
-#endif
-
-#ifndef   __WEAK
-#define __WEAK                                 __attribute__((weak))
-#endif
-
-#ifndef   __PACKED
-#define __PACKED                               __attribute__((packed))
-#endif
-
-#ifndef   __ALIGNED
-#define __ALIGNED(x)                           __attribute__((aligned(x)))
-#endif
-
-#ifndef   __RESTRICT
-#define __RESTRICT                             __restrict
-#endif
-
-#ifndef __NO_INIT
-#define __NO_INIT                              __attribute__ ((section (".bss.noinit"), zero_init))
-#endif
-
-#ifndef __ALIAS
-#define __ALIAS(x)                             __attribute__ ((alias(x)))
-#endif
-
-#ifndef __SECTION
-#define __SECTION(x)                          __attribute__  ((section(x)))
-#endif
-
-#endif // __CC_ARM
 
 
 #ifdef __ICCARM__
--- hbox/hcompiler/hcompiler_armcc.h
+++ hbox/hcompiler/hcompiler_armcc.h
@@ -9,6 +9,60 @@
 #ifndef __HCOMPILER_ARMCC_H__
 #define __HCOMPILER_ARMCC_H__
 
+/*
+Arm Compiler 4/5 (keil mdk)
+*/
+#ifndef   __ASM
+#define __ASM                                  __asm
+#endif
+
+#ifndef   __INLINE
+#define __INLINE                               __inline
+#endif
+
+#ifndef   __STATIC_INLINE
+#define __STATIC_INLINE                        static __inline
+#endif
+
+#ifndef   __STATIC_FORCEINLINE
+#define __STATIC_FORCEINLINE                   static __forceinline
+#endif
+
+#ifndef   __NO_RETURN
+#define __NO_RETURN                            __declspec(noreturn)
+#endif
+
+#ifndef   __USED
+#define __USED                                 __attribute__((used))
+#endif
+
+#ifndef   __WEAK
+#define __WEAK                                 __attribute__((weak))
+#endif
+
+#ifndef   __PACKED
+#define __PACKED                               __attribute__((packed))
+#endif
+
+#ifndef   __ALIGNED
+#define __ALIGNED(x)                           __attribute__((aligned(x)))
+#endif
+
+#ifndef   __RESTRICT
+#define __RESTRICT                             __restrict
+#endif
+
+#ifndef __NO_INIT
+#define __NO_INIT                              __attribute__ ((section (".bss.noinit"), zero_init))
+#endif
+
+#ifndef __ALIAS
+#define __ALIAS(x)                             __attribute__ ((alias(x)))
+#endif
+
+#ifndef __SECTION
+#define __SECTION(x)                          __attribute__  ((section(x)))
+#endif
 
 
 #endif
