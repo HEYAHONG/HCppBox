4b32bbc038a37d0deaa9845d701c0d076241a978
HEYAHONG <2229388563@qq.com>
2025-01-15 14:05:34 +0800

    移动GCC相关宏定义至专用文件

--- hbox/hcompiler.h
+++ hbox/hcompiler.h
@@ -205,61 +205,6 @@ IAR ARM编译器
 
 #endif // __ICCARM__
 
-//#ifdef __GNUC__
-/*
-GCC编译器
-*/
-
-#ifndef   __ASM
-#define __ASM                                  __asm
-#endif
-
-#ifndef   __INLINE
-#define __INLINE                               inline
-#endif
-
-#ifndef   __STATIC_INLINE
-#define __STATIC_INLINE                        static inline
-#endif
-
-#ifndef   __STATIC_FORCEINLINE
-#define __STATIC_FORCEINLINE                   __attribute__((always_inline)) static inline
-#endif
-
-#ifndef   __NO_RETURN
-#define __NO_RETURN                            __attribute__((__noreturn__))
-#endif
-
-#ifndef   __USED
-#define __USED                                 __attribute__((used))
-#endif
-
-#ifndef   __WEAK
-#define __WEAK                                 __attribute__((weak))
-#endif
-
-#ifndef   __PACKED
-#define __PACKED                               __attribute__((packed, aligned(1)))
-#endif
-
-#ifndef   __ALIGNED
-#define __ALIGNED(x)                           __attribute__((aligned(x)))
-#endif
-
-#ifndef   __RESTRICT
-#define __RESTRICT                             __restrict
-#endif
-
-#ifndef __NO_INIT
-#define __NO_INIT                              __attribute__ ((section (".bss.noinit")))
-#endif
-
-
-#ifndef __SECTION
-#define __SECTION(x)                          __attribute__((section(x)))
-#endif
-
-//#endif // __GNUC__
 
 /*
 未使用，通常用于消除编译器警告(未使用的参数)
--- hbox/hcompiler/hcompiler_gcc.h
+++ hbox/hcompiler/hcompiler_gcc.h
@@ -9,6 +9,53 @@
 #ifndef __HCOMPILER_GCC_H__
 #define __HCOMPILER_GCC_H__
 
+#ifndef   __ASM
+#define __ASM                                  __asm
+#endif
 
+#ifndef   __INLINE
+#define __INLINE                               inline
+#endif
+
+#ifndef   __STATIC_INLINE
+#define __STATIC_INLINE                        static inline
+#endif
+
+#ifndef   __STATIC_FORCEINLINE
+#define __STATIC_FORCEINLINE                   __attribute__((always_inline)) static inline
+#endif
+
+#ifndef   __NO_RETURN
+#define __NO_RETURN                            __attribute__((__noreturn__))
+#endif
+
+#ifndef   __USED
+#define __USED                                 __attribute__((used))
+#endif
+
+#ifndef   __WEAK
+#define __WEAK                                 __attribute__((weak))
+#endif
+
+#ifndef   __PACKED
+#define __PACKED                               __attribute__((packed, aligned(1)))
+#endif
+
+#ifndef   __ALIGNED
+#define __ALIGNED(x)                           __attribute__((aligned(x)))
+#endif
+
+#ifndef   __RESTRICT
+#define __RESTRICT                             __restrict
+#endif
+
+#ifndef __NO_INIT
+#define __NO_INIT                              __attribute__ ((section (".bss.noinit")))
+#endif
+
+
+#ifndef __SECTION
+#define __SECTION(x)                          __attribute__((section(x)))
+#endif
 
 #endif
