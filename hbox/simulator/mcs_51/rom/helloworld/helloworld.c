477d17bb48934e85cf3d7ca27d860cf9148e61d2
HEYAHONG <2229388563@qq.com>
2025-02-13 21:49:32 +0800

    修复sdcc编译警告

--- hbox/simulator/mcs_51/rom/helloworld/helloworld.c
+++ hbox/simulator/mcs_51/rom/helloworld/helloworld.c
@@ -3,31 +3,31 @@
 #include "8051.h"
 
 //外部中断0
-void ie0_isr() __interrupt(IE0_VECTOR)
+void ie0_isr(void) __interrupt(IE0_VECTOR)
 {
 
 }
 
 //定时器0
-void tf0_isr() __interrupt(TF0_VECTOR)
+void tf0_isr(void) __interrupt(TF0_VECTOR)
 {
 
 }
 
 //外部中断1
-void ie1_isr() __interrupt(IE1_VECTOR)
+void ie1_isr(void) __interrupt(IE1_VECTOR)
 {
 
 }
 
 //定时器1
-void tf1_isr() __interrupt(TF1_VECTOR)
+void tf1_isr(void) __interrupt(TF1_VECTOR)
 {
 
 }
 
 //串口0
-void si0_isr() __interrupt(SI0_VECTOR)
+void si0_isr(void) __interrupt(SI0_VECTOR)
 {
 
 }
@@ -50,7 +50,7 @@ void uart_send_char(int ch)
     TI=0;
 }
 
-uint8_t uart_receive_char()
+uint8_t uart_receive_char(void)
 {
     /*
      * 注意：此代码要求实现串口功能，即接收数据时RI需要被硬件（模拟器）置1。
