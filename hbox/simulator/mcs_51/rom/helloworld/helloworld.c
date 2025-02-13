c08c123c9f985b9a3ab2e92eda145da832e0379e
HEYAHONG <2229388563@qq.com>
2025-02-13 17:40:37 +0800

    完善MCS-51示例程序

--- hbox/simulator/mcs_51/rom/helloworld/helloworld.c
+++ hbox/simulator/mcs_51/rom/helloworld/helloworld.c
@@ -2,6 +2,39 @@
 #include "stdint.h"
 #include "8051.h"
 
+//外部中断0
+void ie0_isr() __interrupt(IE0_VECTOR)
+{
+
+}
+
+//定时器0
+void tf0_isr() __interrupt(TF0_VECTOR)
+{
+
+}
+
+//外部中断1
+void ie1_isr() __interrupt(IE1_VECTOR)
+{
+
+}
+
+//定时器1
+void tf1_isr() __interrupt(TF1_VECTOR)
+{
+
+}
+
+//串口0
+void si0_isr() __interrupt(SI0_VECTOR)
+{
+
+}
+
+
+
+
 void uart_init(void)
 {
     SCON=0x90;
@@ -17,6 +50,16 @@ void uart_send_char(int ch)
     TI=0;
 }
 
+uint8_t uart_receive_char()
+{
+    /*
+     * 注意：此代码要求实现串口功能，即接收数据时RI需要被硬件（模拟器）置1。
+     */
+    while(RI==0);
+    RI=0;
+    return SBUF;
+}
+
 void uart_send_str(char *str)
 {
     while(*str!='\0')
@@ -29,5 +72,10 @@ void main(void)
 {
     uart_init();
     uart_send_str("Hello World!\n");
-    while(1);
+    while(1)
+    {
+        int data=uart_receive_char();
+        uart_send_char(data);
+    }
 }
+
