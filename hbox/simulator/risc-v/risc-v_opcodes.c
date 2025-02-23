bbb0254580270b5cc66bbeb8fedb2a51740d0c16
HEYAHONG <2229388563@qq.com>
2025-02-23 22:51:39 +0800

    添加RISC-V RV32异常CAUSE

--- hbox/simulator/risc-v/risc-v_opcodes.c
+++ hbox/simulator/risc-v/risc-v_opcodes.c
@@ -17,6 +17,7 @@ extern const hs_risc_v_opcodes_insn_t hs_risc_v_opcodes_insn_##NAME;\
 const hs_risc_v_opcodes_insn_t hs_risc_v_opcodes_insn_##NAME={#NAME,MATCH,MASK};
 #endif // DECLARE_INSN
 
+
 /*
  * 用于声明CSR
  */
@@ -24,7 +25,16 @@ const hs_risc_v_opcodes_insn_t hs_risc_v_opcodes_insn_##NAME={#NAME,MATCH,MASK};
 #define DECLARE_CSR(NAME,ADDR)   \
 extern const hs_risc_v_opcodes_csr_t hs_risc_v_opcodes_csr_##NAME;\
 const hs_risc_v_opcodes_csr_t hs_risc_v_opcodes_csr_##NAME={#NAME,ADDR};
-#endif // DECLARE_INSN
+#endif // DECLARE_CSR
+
+/*
+ * 用于声明异常CAUSE
+ */
+#ifndef DECLARE_CAUSE
+#define DECLARE_CAUSE(NAME,VALUE)   \
+HS_RISC_V_OPCODES_EXCEPTION_DECLARE(VALUE);\
+const int HS_RISC_V_OPCODES_EXCEPTION_##VALUE=VALUE;
+#endif // DECLARE_CAUSE
 
 /*
  *  由riscv-opcodes（https://github.com/riscv/riscv-opcodes.git）生成的C语言头文件。
