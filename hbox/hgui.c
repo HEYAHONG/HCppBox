a948b5668dceb7c77e577968ae021f0a4483c9f9
HEYAHONG <2229388563@qq.com>
2025-03-15 02:23:52 +0800

    优化hgui组件结构

---
+++ hbox/hgui.c
@@ -0,0 +1,25 @@
+/***************************************************************
+ * Name:      hgui.c
+ * Purpose:   实现hgui接口
+ * Author:    HYH (hyhsystem.cn)
+ * Created:   2025-03-15
+ * Copyright: HYH (hyhsystem.cn)
+ * License:   MIT
+ **************************************************************/
+#include "hgui.h"
+
+/*
+ * 基础组件
+ */
+#include "gui/hgui_gui_common.c"
+#include "gui/hgui_gui_dotfont.c"
+#include "gui/hgui_gui_driver.c"
+#include "gui/hgui_gui_event.c"
+#include "gui/hgui_gui_pixel.c"
+#include "gui/hgui_gui_xpm.c"
+
+/*
+ * 场景1
+ */
+#include "gui/hgui_gui_scene1.c"
+
