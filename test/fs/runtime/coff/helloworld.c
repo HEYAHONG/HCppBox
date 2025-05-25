abad4fe0abe0282e424ab8a05804541de3e69c7e
HEYAHONG <2229388563@qq.com>
2025-05-25 20:02:21 +0800

    [hcoff] 更新AARCH64测试文件

--- test/fs/runtime/coff/helloworld.c
+++ test/fs/runtime/coff/helloworld.c
@@ -14,8 +14,9 @@ const struct
 };
  /*
   * x86_64模式下，编译器生成的32位相对地址可能会溢出，故而不直接调用外部函数，如需调用应使用函数指针,且函数的参数也应当尽量使用指针
+  * AARCH64同x86_64 
   */
-#if defined(_M_X64) || defined(__x86_64) || defined(__x86_64__)
+#if defined(_M_X64) || defined(__x86_64) || defined(__x86_64__) || defined(__aarch64__) || defined(__AARCH64EL__)
 int (*hprintf)(const char *fmt,...)=0;
 #else
 extern int hprintf(const char *fmt,...);
