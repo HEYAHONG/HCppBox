da63b8dbefbf089fe6ad0b2b0bfc4bda7f65b0b6
HEYAHONG <2229388563@qq.com>
2025-06-10 14:24:35 +0800

    [hexecutableloader] 完善代码

--- hbox/hruntime/hexecutableloader/helf/helf_common.c
+++ hbox/hruntime/hexecutableloader/helf/helf_common.c
@@ -99,3 +99,241 @@ bool helf_file_input_is_big_endian(helf_file_input_t *input_file)
     ret=(HELF_ELFDATA_2_MSB==helf_elfdata_get(ident));
     return ret;
 }
+
+
+bool helf_file_input_is_32_bits(helf_file_input_t *input_file)
+{
+    bool ret=false;
+    if(input_file==NULL)
+    {
+        return ret;
+    }
+    helf_e_ident_t ident= {0};
+    if(helf_file_input_read(input_file,0,ident,sizeof(ident)) < sizeof(ident))
+    {
+        return ret;
+    }
+    ret=(HELF_ELFCLASS_32==helf_elfclass_get(ident));
+    return ret;
+}
+
+typedef struct
+{
+    uint8_t e_ident[16];        /* ELF "magic number" */
+    uint8_t e_type[2];          /* Identifies object file type */
+    uint8_t e_machine[2];       /* Specifies required architecture */
+    uint8_t e_version[4];       /* Identifies object file version */
+    uint8_t e_entry[4];         /* Entry point virtual address */
+    uint8_t e_phoff[4];         /* Program header table file offset */
+    uint8_t e_shoff[4];         /* Section header table file offset */
+    uint8_t e_flags[4];         /* Processor-specific flags */
+    uint8_t e_ehsize[2];        /* ELF header size in bytes */
+    uint8_t e_phentsize[2];     /* Program header table entry size */
+    uint8_t e_phnum[2];         /* Program header table entry count */
+    uint8_t e_shentsize[2];     /* Section header table entry size */
+    uint8_t e_shnum[2];         /* Section header table entry count */
+    uint8_t e_shstrndx[2];      /* Section header string table index */
+} helf_elf32_file_header_bytes_t;
+
+
+bool helf_file_input_32_bits_header_get(helf_file_input_t *input_file,helf_elf32_file_header_t *header)
+{
+    bool ret=false;
+    if(header==NULL)
+    {
+        return ret;
+    }
+    if(!helf_file_input_is_32_bits(input_file))
+    {
+        return ret;
+    }
+    helf_elf32_file_header_bytes_t header_bytes= {0};
+    if(helf_file_input_read(input_file,0,&header_bytes,sizeof(header_bytes)) < sizeof(header_bytes))
+    {
+        return ret;
+    }
+    ret=true;
+    memcpy(header->e_ident,header_bytes.e_ident,sizeof(header_bytes.e_ident));
+    if(helf_file_input_is_big_endian(input_file))
+    {
+#ifdef HELF_READ_16
+#undef HELF_READ_16
+#endif // HELF_READ_16
+#ifdef HELF_READ_32
+#undef HELF_READ_32
+#endif // HELF_READ_32
+#define HELF_READ_16(v,a) v=a[0]*(1ULL << 8)+a[1]*(1ULL << 0)
+#define HELF_READ_32(v,a) v=a[0]*(1ULL << 24)+a[1]*(1ULL << 16)+a[2]*(1ULL << 8)+a[3]*(1ULL << 0)
+
+        HELF_READ_16(header->e_type,header_bytes.e_type);
+        HELF_READ_16(header->e_machine,header_bytes.e_machine);
+        HELF_READ_32(header->e_version,header_bytes.e_version);
+        HELF_READ_32(header->e_entry,header_bytes.e_entry);
+        HELF_READ_32(header->e_phoff,header_bytes.e_phoff);
+        HELF_READ_32(header->e_shoff,header_bytes.e_shoff);
+        HELF_READ_32(header->e_flags,header_bytes.e_flags);
+        HELF_READ_16(header->e_ehsize,header_bytes.e_ehsize);
+        HELF_READ_16(header->e_phentsize,header_bytes.e_phentsize);
+        HELF_READ_16(header->e_phnum,header_bytes.e_phnum);
+        HELF_READ_16(header->e_shentsize,header_bytes.e_shentsize);
+        HELF_READ_16(header->e_shnum,header_bytes.e_shnum);
+        HELF_READ_16(header->e_shstrndx,header_bytes.e_shstrndx);
+
+#undef HELF_READ_16
+#undef HELF_READ_32
+    }
+    else
+    {
+#ifdef HELF_READ_16
+#undef HELF_READ_16
+#endif // HELF_READ_16
+#ifdef HELF_READ_32
+#undef HELF_READ_32
+#endif // HELF_READ_32
+#define HELF_READ_16(v,a) v=a[1]*(1ULL << 8)+a[0]*(1ULL << 0)
+#define HELF_READ_32(v,a) v=a[3]*(1ULL << 24)+a[2]*(1ULL << 16)+a[1]*(1ULL << 8)+a[0]*(1ULL << 0)
+
+        HELF_READ_16(header->e_type,header_bytes.e_type);
+        HELF_READ_16(header->e_machine,header_bytes.e_machine);
+        HELF_READ_32(header->e_version,header_bytes.e_version);
+        HELF_READ_32(header->e_entry,header_bytes.e_entry);
+        HELF_READ_32(header->e_phoff,header_bytes.e_phoff);
+        HELF_READ_32(header->e_shoff,header_bytes.e_shoff);
+        HELF_READ_32(header->e_flags,header_bytes.e_flags);
+        HELF_READ_16(header->e_ehsize,header_bytes.e_ehsize);
+        HELF_READ_16(header->e_phentsize,header_bytes.e_phentsize);
+        HELF_READ_16(header->e_phnum,header_bytes.e_phnum);
+        HELF_READ_16(header->e_shentsize,header_bytes.e_shentsize);
+        HELF_READ_16(header->e_shnum,header_bytes.e_shnum);
+        HELF_READ_16(header->e_shstrndx,header_bytes.e_shstrndx);
+
+#undef HELF_READ_16
+#undef HELF_READ_32
+    }
+    return ret;
+}
+
+
+bool helf_file_input_is_64_bits(helf_file_input_t *input_file)
+{
+    bool ret=false;
+    if(input_file==NULL)
+    {
+        return ret;
+    }
+    helf_e_ident_t ident= {0};
+    if(helf_file_input_read(input_file,0,ident,sizeof(ident)) < sizeof(ident))
+    {
+        return ret;
+    }
+    ret=(HELF_ELFCLASS_64==helf_elfclass_get(ident));
+    return ret;
+}
+
+typedef struct
+{
+    uint8_t e_ident[16];        /* ELF "magic number" */
+    uint8_t e_type[2];          /* Identifies object file type */
+    uint8_t e_machine[2];       /* Specifies required architecture */
+    uint8_t e_version[4];       /* Identifies object file version */
+    uint8_t e_entry[8];         /* Entry point virtual address */
+    uint8_t e_phoff[8];         /* Program header table file offset */
+    uint8_t e_shoff[8];         /* Section header table file offset */
+    uint8_t e_flags[4];         /* Processor-specific flags */
+    uint8_t e_ehsize[2];        /* ELF header size in bytes */
+    uint8_t e_phentsize[2];     /* Program header table entry size */
+    uint8_t e_phnum[2];         /* Program header table entry count */
+    uint8_t e_shentsize[2];     /* Section header table entry size */
+    uint8_t e_shnum[2];         /* Section header table entry count */
+    uint8_t e_shstrndx[2];      /* Section header string table index */
+} helf_elf64_file_header_bytes_t;
+
+bool helf_file_input_64_bits_header_get(helf_file_input_t *input_file,helf_elf64_file_header_t *header)
+{
+    bool ret=false;
+    if(header==NULL)
+    {
+        return ret;
+    }
+    if(!helf_file_input_is_64_bits(input_file))
+    {
+        return ret;
+    }
+    helf_elf64_file_header_bytes_t header_bytes= {0};
+    if(helf_file_input_read(input_file,0,&header_bytes,sizeof(header_bytes)) < sizeof(header_bytes))
+    {
+        return ret;
+    }
+    ret=true;
+    memcpy(header->e_ident,header_bytes.e_ident,sizeof(header_bytes.e_ident));
+    if(helf_file_input_is_big_endian(input_file))
+    {
+#ifdef HELF_READ_16
+#undef HELF_READ_16
+#endif // HELF_READ_16
+#ifdef HELF_READ_32
+#undef HELF_READ_32
+#endif // HELF_READ_32
+#ifdef HELF_READ_64
+#undef HELF_READ_64
+#endif // HELF_READ_64
+#define HELF_READ_16(v,a) v=a[0]*(1ULL << 8)+a[1]*(1ULL << 0)
+#define HELF_READ_32(v,a) v=a[0]*(1ULL << 24)+a[1]*(1ULL << 16)+a[2]*(1ULL << 8)+a[3]*(1ULL << 0)
+#define HELF_READ_64(v,a) v=a[0]*(1ULL << 56)+a[1]*(1ULL << 48)+a[2]*(1ULL << 40)+a[3]*(1ULL << 32)+a[4]*(1ULL << 24)+a[5]*(1ULL << 16)+a[6]*(1ULL << 8)+a[7]*(1ULL << 0)
+
+
+        HELF_READ_16(header->e_type,header_bytes.e_type);
+        HELF_READ_16(header->e_machine,header_bytes.e_machine);
+        HELF_READ_32(header->e_version,header_bytes.e_version);
+        HELF_READ_64(header->e_entry,header_bytes.e_entry);
+        HELF_READ_64(header->e_phoff,header_bytes.e_phoff);
+        HELF_READ_64(header->e_shoff,header_bytes.e_shoff);
+        HELF_READ_32(header->e_flags,header_bytes.e_flags);
+        HELF_READ_16(header->e_ehsize,header_bytes.e_ehsize);
+        HELF_READ_16(header->e_phentsize,header_bytes.e_phentsize);
+        HELF_READ_16(header->e_phnum,header_bytes.e_phnum);
+        HELF_READ_16(header->e_shentsize,header_bytes.e_shentsize);
+        HELF_READ_16(header->e_shnum,header_bytes.e_shnum);
+        HELF_READ_16(header->e_shstrndx,header_bytes.e_shstrndx);
+
+#undef HELF_READ_16
+#undef HELF_READ_32
+#undef HELF_READ_64
+    }
+    else
+    {
+#ifdef HELF_READ_16
+#undef HELF_READ_16
+#endif // HELF_READ_16
+#ifdef HELF_READ_32
+#undef HELF_READ_32
+#endif // HELF_READ_32
+#ifdef HELF_READ_64
+#undef HELF_READ_64
+#endif // HELF_READ_64
+#define HELF_READ_16(v,a) v=a[1]*(1ULL << 8)+a[0]*(1ULL << 0)
+#define HELF_READ_32(v,a) v=a[3]*(1ULL << 24)+a[2]*(1ULL << 16)+a[1]*(1ULL << 8)+a[0]*(1ULL << 0)
+#define HELF_READ_64(v,a) v=a[7]*(1ULL << 56)+a[6]*(1ULL << 48)+a[5]*(1ULL << 40)+a[4]*(1ULL << 32)+a[3]*(1ULL << 24)+a[2]*(1ULL << 16)+a[1]*(1ULL << 8)+a[0]*(1ULL << 0)
+
+        HELF_READ_16(header->e_type,header_bytes.e_type);
+        HELF_READ_16(header->e_machine,header_bytes.e_machine);
+        HELF_READ_32(header->e_version,header_bytes.e_version);
+        HELF_READ_64(header->e_entry,header_bytes.e_entry);
+        HELF_READ_64(header->e_phoff,header_bytes.e_phoff);
+        HELF_READ_64(header->e_shoff,header_bytes.e_shoff);
+        HELF_READ_32(header->e_flags,header_bytes.e_flags);
+        HELF_READ_16(header->e_ehsize,header_bytes.e_ehsize);
+        HELF_READ_16(header->e_phentsize,header_bytes.e_phentsize);
+        HELF_READ_16(header->e_phnum,header_bytes.e_phnum);
+        HELF_READ_16(header->e_shentsize,header_bytes.e_shentsize);
+        HELF_READ_16(header->e_shnum,header_bytes.e_shnum);
+        HELF_READ_16(header->e_shstrndx,header_bytes.e_shstrndx);
+
+#undef HELF_READ_16
+#undef HELF_READ_32
+#undef HELF_READ_64
+    }
+    return ret;
+}
+
+
--- hbox/hruntime/hexecutableloader/helf/helf_common.h
+++ hbox/hruntime/hexecutableloader/helf/helf_common.h
@@ -174,6 +174,194 @@ size_t helf_file_input_read(helf_file_input_t *input,uintptr_t address,void *buf
  */
 bool helf_file_input_is_big_endian(helf_file_input_t *input_file);
 
+/** \brief ELF文件是否为32位
+ *
+ * \param input helf_file_input_t* 输入文件
+ * \return bool 是否为32位
+ *
+ */
+bool helf_file_input_is_32_bits(helf_file_input_t *input_file);
+
+typedef struct helf_elf32_file_header helf_elf32_file_header_t;
+struct helf_elf32_file_header
+{
+    helf_e_ident_t      e_ident;        /* ELF "magic number" */
+    uint16_t            e_type;         /* Identifies object file type */
+    uint16_t            e_machine;      /* Specifies required architecture */
+    uint32_t            e_version;      /* Identifies object file version */
+    uint32_t            e_entry;        /* Entry point virtual address */
+    uint32_t            e_phoff;        /* Program header table file offset */
+    uint32_t            e_shoff;        /* Section header table file offset */
+    uint32_t            e_flags;        /* Processor-specific flags */
+    uint16_t            e_ehsize;       /* ELF header size in bytes */
+    uint16_t            e_phentsize;    /* Program header table entry size */
+    uint16_t            e_phnum;        /* Program header table entry count */
+    uint16_t            e_shentsize;    /* Section header table entry size */
+    uint16_t            e_shnum;        /* Section header table entry count */
+    uint16_t            e_shstrndx;     /* Section header string table index */
+};
+
+/** \brief ELF读取32位头
+ *
+ * \param input_file helf_file_input_t* 输入文件
+ * \param header helf_elf32_file_header_t* 32位文件头
+ * \return bool 是否读取成功
+ *
+ */
+bool helf_file_input_32_bits_header_get(helf_file_input_t *input_file,helf_elf32_file_header_t *header);
+
+/** \brief ELF文件是否为64位
+ *
+ * \param input helf_file_input_t* 输入文件
+ * \return bool 是否为64位
+ *
+ */
+bool helf_file_input_is_64_bits(helf_file_input_t *input_file);
+
+typedef struct helf_elf64_file_header helf_elf64_file_header_t;
+struct helf_elf64_file_header
+{
+    helf_e_ident_t  e_ident;            /* ELF "magic number" */
+    uint16_t        e_type;             /* Identifies object file type */
+    uint16_t        e_machine;          /* Specifies required architecture */
+    uint32_t        e_version;          /* Identifies object file version */
+    uint64_t        e_entry;            /* Entry point virtual address */
+    uint64_t        e_phoff;            /* Program header table file offset */
+    uint64_t        e_shoff;            /* Section header table file offset */
+    uint32_t        e_flags;            /* Processor-specific flags */
+    uint16_t        e_ehsize;           /* ELF header size in bytes */
+    uint16_t        e_phentsize;        /* Program header table entry size */
+    uint16_t        e_phnum;            /* Program header table entry count */
+    uint16_t        e_shentsize;        /* Section header table entry size */
+    uint16_t        e_shnum;            /* Section header table entry count */
+    uint16_t        e_shstrndx;         /* Section header string table index */
+};
+
+/** \brief ELF读取64位头
+ *
+ * \param input_file helf_file_input_t* 输入文件
+ * \param header helf_elf64_file_header_t* 64位文件头
+ * \return bool 是否读取成功
+ *
+ */
+bool helf_file_input_64_bits_header_get(helf_file_input_t *input_file,helf_elf64_file_header_t *header);
+
+/*
+ * e_type
+ */
+#define HELF_ET_NONE        0           /* Unknown type. */
+#define HELF_ET_REL         1           /* Relocatable. */
+#define HELF_ET_EXEC        2           /* Executable. */
+#define HELF_ET_DYN         3           /* Shared object. */
+#define HELF_ET_CORE        4           /* Core file. */
+#define HELF_ET_LOOS        0xfe00      /* First operating system specific. */
+#define HELF_ET_HIOS        0xfeff      /* Last operating system-specific. */
+#define HELF_ET_LOPROC      0xff00      /* First processor-specific. */
+#define HELF_ET_HIPROC      0xffff      /* Last processor-specific. */
+
+/*
+ * e_machine
+ */
+#define HELF_EM_NONE        0               /* Unknown machine. */
+#define HELF_EM_M32         1               /* AT&T WE32100. */
+#define HELF_EM_SPARC       2               /* Sun SPARC. */
+#define HELF_EM_386         3               /* Intel i386. */
+#define HELF_EM_68K         4               /* Motorola 68000. */
+#define HELF_EM_88K         5               /* Motorola 88000. */
+#define HELF_EM_IAMCU       6               /* Intel MCU. */
+#define HELF_EM_860         7               /* Intel i860. */
+#define HELF_EM_MIPS        8               /* MIPS R3000 Big-Endian only. */
+#define HELF_EM_S370        9               /* IBM System/370. */
+#define HELF_EM_MIPS_RS3_LE 10              /* MIPS R3000 Little-Endian. */
+#define HELF_EM_PARISC      15              /* HP PA-RISC. */
+#define HELF_EM_VPP500      17              /* Fujitsu VPP500. */
+#define HELF_EM_SPARC32PLUS 18              /* SPARC v8plus. */
+#define HELF_EM_960         19              /* Intel 80960. */
+#define HELF_EM_PPC         20              /* PowerPC 32-bit. */
+#define HELF_EM_PPC64       21              /* PowerPC 64-bit. */
+#define HELF_EM_S390        22              /* IBM System/390. */
+#define HELF_EM_V800        36              /* NEC V800. */
+#define HELF_EM_FR20        37              /* Fujitsu FR20. */
+#define HELF_EM_RH32        38              /* TRW RH-32. */
+#define HELF_EM_RCE         39              /* Motorola RCE. */
+#define HELF_EM_ARM         40              /* ARM. */
+#define HELF_EM_SH          42              /* Hitachi SH. */
+#define HELF_EM_SPARCV9     43              /* SPARC v9 64-bit. */
+#define HELF_EM_TRICORE     44              /* Siemens TriCore embedded processor. */
+#define HELF_EM_ARC         45              /* Argonaut RISC Core. */
+#define HELF_EM_H8_300      46              /* Hitachi H8/300. */
+#define HELF_EM_H8_300H     47              /* Hitachi H8/300H. */
+#define HELF_EM_H8S         48              /* Hitachi H8S. */
+#define HELF_EM_H8_500      49              /* Hitachi H8/500. */
+#define HELF_EM_IA_64       50              /* Intel IA-64 Processor. */
+#define HELF_EM_MIPS_X      51              /* Stanford MIPS-X. */
+#define HELF_EM_COLDFIRE    52              /* Motorola ColdFire. */
+#define HELF_EM_68HC12      53              /* Motorola M68HC12. */
+#define HELF_EM_MMA         54              /* Fujitsu MMA. */
+#define HELF_EM_PCP         55              /* Siemens PCP. */
+#define HELF_EM_NCPU        56              /* Sony nCPU. */
+#define HELF_EM_NDR1        57              /* Denso NDR1 microprocessor. */
+#define HELF_EM_STARCORE    58              /* Motorola Star*Core processor. */
+#define HELF_EM_ME16        59              /* Toyota ME16 processor. */
+#define HELF_EM_ST100       60              /* STMicroelectronics ST100 processor. */
+#define HELF_EM_TINYJ       61              /* Advanced Logic Corp. TinyJ processor. */
+#define HELF_EM_X86_64      62              /* Advanced Micro Devices x86-64 */
+#define HELF_EM_AMD64       HELF_EM_X86_64  /* Advanced Micro Devices x86-64 (compat) */
+#define HELF_EM_PDSP        63              /* Sony DSP Processor. */
+#define HELF_EM_FX66        66              /* Siemens FX66 microcontroller. */
+#define HELF_EM_ST9PLUS     67              /* STMicroelectronics ST9+ 8/16 microcontroller. */
+#define HELF_EM_ST7         68              /* STmicroelectronics ST7 8-bit  microcontroller. */
+#define HELF_EM_68HC16      69              /* Motorola MC68HC16 microcontroller. */
+#define HELF_EM_68HC11      70              /* Motorola MC68HC11 microcontroller. */
+#define HELF_EM_68HC08      71              /* Motorola MC68HC08 microcontroller. */
+#define HELF_EM_68HC05      72              /* Motorola MC68HC05 microcontroller. */
+#define HELF_EM_SVX         73              /* Silicon Graphics SVx. */
+#define HELF_EM_ST19        74              /* STMicroelectronics ST19 8-bit mc. */
+#define HELF_EM_VAX         75              /* Digital VAX. */
+#define HELF_EM_CRIS        76              /* Axis Communications 32-bit embedded   processor. */
+#define HELF_EM_JAVELIN     77              /* Infineon Technologies 32-bit embedded processor. */
+#define HELF_EM_FIREPATH    78              /* Element 14 64-bit DSP Processor. */
+#define HELF_EM_ZSP         79              /* LSI Logic 16-bit DSP Processor. */
+#define HELF_EM_MMIX        80              /* Donald Knuth's educational 64-bit proc. */
+#define HELF_EM_HUANY       81              /* Harvard University machine-independent  object files. */
+#define HELF_EM_PRISM       82              /* SiTera Prism. */
+#define HELF_EM_AVR         83              /* Atmel AVR 8-bit microcontroller. */
+#define HELF_EM_FR30        84              /* Fujitsu FR30. */
+#define HELF_EM_D10V        85              /* Mitsubishi D10V. */
+#define HELF_EM_D30V        86              /* Mitsubishi D30V. */
+#define HELF_EM_V850        87              /* NEC v850. */
+#define HELF_EM_M32R        88              /* Mitsubishi M32R. */
+#define HELF_EM_MN10300     89              /* Matsushita MN10300. */
+#define HELF_EM_MN10200     90              /* Matsushita MN10200. */
+#define HELF_EM_PJ          91              /* picoJava. */
+#define HELF_EM_OPENRISC    92              /* OpenRISC 32-bit embedded processor. */
+#define HELF_EM_ARC_A5      93              /* ARC Cores Tangent-A5. */
+#define HELF_EM_XTENSA      94              /* Tensilica Xtensa Architecture. */
+#define HELF_EM_VIDEOCORE   95              /* Alphamosaic VideoCore processor. */
+#define HELF_EM_TMM_GPP     96              /* Thompson Multimedia General Purpose   Processor. */
+#define HELF_EM_NS32K       97              /* National Semiconductor 32000 series. */
+#define HELF_EM_TPC         98              /* Tenor Network TPC processor. */
+#define HELF_EM_SNP1K       99              /* Trebia SNP 1000 processor. */
+#define HELF_EM_ST200       100             /* STMicroelectronics ST200 microcontroller. */
+#define HELF_EM_IP2K        101             /* Ubicom IP2xxx microcontroller family. */
+#define HELF_EM_MAX         102             /* MAX Processor. */
+#define HELF_EM_CR          103             /* National Semiconductor CompactRISC  microprocessor. */
+#define HELF_EM_F2MC16      104             /* Fujitsu F2MC16. */
+#define HELF_EM_MSP430      105             /* Texas Instruments embedded microcontroller  msp430. */
+#define HELF_EM_BLACKFIN    106             /* Analog Devices Blackfin (DSP) processor. */
+#define HELF_EM_SE_C33      107             /* S1C33 Family of Seiko Epson processors. */
+#define HELF_EM_SEP         108             /* Sharp embedded microprocessor. */
+#define HELF_EM_ARCA        109             /* Arca RISC Microprocessor. */
+#define HELF_EM_UNICORE     110             /* Microprocessor series from PKU-Unity Ltd. and MPRC of Peking University */
+#define HELF_EM_AARCH64     183             /* AArch64 (64-bit ARM) */
+#define HELF_EM_RISCV       243             /* RISC-V */
+/* Non-standard or deprecated. */
+#define HELF_EM_486         6               /* Intel i486. */
+#define HELF_EM_MIPS_RS4_BE 10              /* MIPS R4000 Big-Endian */
+#define HELF_EM_ALPHA_STD   41              /* Digital Alpha (standard value). */
+#define HELF_EM_ALPHA       0x9026          /* Alpha (written in the absence of an ABI) */
+
+
 
 #ifdef __cplusplus
 }
