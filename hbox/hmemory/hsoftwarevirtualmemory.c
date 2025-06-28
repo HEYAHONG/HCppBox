f481d68be40c6ac68d5812d18430e8c64656d945
HEYAHONG <2229388563@qq.com>
2025-06-28 23:47:28 +0800

    [hmemory] 修复BUG

--- hbox/hmemory/hsoftwarevirtualmemory.c
+++ hbox/hmemory/hsoftwarevirtualmemory.c
@@ -15,7 +15,7 @@ size_t hsoftwarevirtualmemory_read(const hsoftwarevirtualmemory_map_item_t *map_
     {
         return ret;
     }
-
+    const hsoftwarevirtualmemory_map_item_t * map_table_start=map_table;
     while(true)
     {
         if(map_table->map_address == 0 && map_table->map_size==0)
@@ -26,11 +26,20 @@ size_t hsoftwarevirtualmemory_read(const hsoftwarevirtualmemory_map_item_t *map_
             }
             break;
         }
-        if(((uintptr_t)data) >= map_table->map_address && ((uintptr_t)data) < (map_table->map_address+map_table->map_size))
+        if(((uintptr_t)address) >= map_table->map_address && ((uintptr_t)address) < (map_table->map_address+map_table->map_size))
         {
             if(map_table->read_callback!=NULL)
             {
-                ret=map_table->read_callback(map_table,address,data,length);
+                size_t datalength=length;
+                if(address+datalength > (map_table->map_address+map_table->map_size))
+                {
+                    datalength=(map_table->map_address+map_table->map_size)-address;
+                }
+                ret=map_table->read_callback(map_table,address,data,datalength);
+                if(datalength < length)
+                {
+                    ret+=hsoftwarevirtualmemory_read(map_table_start,address+datalength,&data[datalength],length-datalength);
+                }
             }
             break;
         }
@@ -46,7 +55,7 @@ size_t hsoftwarevirtualmemory_write(const hsoftwarevirtualmemory_map_item_t *map
     {
         return ret;
     }
-
+    const hsoftwarevirtualmemory_map_item_t * map_table_start=map_table;
     while(true)
     {
         if(map_table->map_address == 0 && map_table->map_size==0)
@@ -57,11 +66,20 @@ size_t hsoftwarevirtualmemory_write(const hsoftwarevirtualmemory_map_item_t *map
             }
             break;
         }
-        if(((uintptr_t)data) >= map_table->map_address && ((uintptr_t)data) < (map_table->map_address+map_table->map_size))
+        if(((uintptr_t)address) >= map_table->map_address && ((uintptr_t)address) < (map_table->map_address+map_table->map_size))
         {
             if(map_table->write_callback!=NULL)
             {
-                ret=map_table->write_callback(map_table,address,data,length);
+                size_t datalength=length;
+                if(address+datalength > (map_table->map_address+map_table->map_size))
+                {
+                    datalength=(map_table->map_address+map_table->map_size)-address;
+                }
+                ret=map_table->write_callback(map_table,address,data,datalength);
+                if(datalength < length)
+                {
+                    ret+=hsoftwarevirtualmemory_write(map_table_start,address+datalength,&data[datalength],length-datalength);
+                }
             }
             break;
         }
