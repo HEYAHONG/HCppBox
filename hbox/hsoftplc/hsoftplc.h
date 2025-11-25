aaeff0c6669897b2c6a9edcbf7c63feed55c167d
HEYAHONG <2229388563@qq.com>
2025-11-25 09:30:42 +0800

    [hsoftplc] 初步添加动态链接库相关函数（默认导出所有符号）

--- hbox/hsoftplc/hsoftplc.h
+++ hbox/hsoftplc/hsoftplc.h
@@ -122,6 +122,37 @@ struct hsoftplc_variable_symbol
  */
 hsoftplc_variable_symbol_t * hsoftplc_parse_variable_symbol(hsoftplc_variable_symbol_t * variable_symbol,const char *variable_name_or_iec_addr);
 
+
+struct hsoftplc_public_interface;
+typedef struct hsoftplc_public_interface hsoftplc_public_interface_t;                                           /**< 软件PLC接口，用于导出动态链接库 */
+struct hsoftplc_public_interface
+{
+    size_t interface_length;                                                                                    /**< 接口大小，用于校验 */
+    void (*interface_init)(void);                                                                               /**< 接口初始化 */
+    void (*interface_loop)(void);                                                                               /**< 接口循环 */
+    hsoftplc_callback_t (*interface_set_callback)(hsoftplc_callback_t cb);                                      /**< 接口设置回调 */
+    size_t (*interface_get_located_all_variables)(hsoftplc_located_variable_enum_callback_t cb,void *usr);      /**< 接口枚举变量 */
+};
+
+#define HSOFTPLC_PUBLIC_INTERFACE_INIT_DATA     \
+    {\
+        sizeof(hsoftplc_public_interface_t),\
+        hsoftplc_init,\
+        hsoftplc_loop,\
+        hsoftplc_set_callback,\
+        hsoftplc_get_located_all_variables,\
+    }\
+
+#ifdef __cplusplus
+#define HSOFTPLC_EXPORT_INTERFACE() \
+extern "C" hsoftplc_public_interface_t hsoftplc_interface =\
+HSOFTPLC_PUBLIC_INTERFACE_INIT_DATA
+#else
+#define HSOFTPLC_EXPORT_INTERFACE() \
+hsoftplc_public_interface_t hsoftplc_interface =\
+HSOFTPLC_PUBLIC_INTERFACE_INIT_DATA
+#endif // __cplusplus
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
