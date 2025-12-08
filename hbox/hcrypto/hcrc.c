cf457f41baf6f99d0b8a8af48adf5bc11d1699d1
HEYAHONG <2229388563@qq.com>
2025-12-08 16:47:47 +0800

    [hcrc] 完善接口

--- hbox/hcrypto/hcrc.c
+++ hbox/hcrypto/hcrc.c
@@ -44,6 +44,15 @@ const hcrc_crc8_t  hcrc_crc8_maxim=
     true
 };
 
+hcrc_crc8_context_t hcrc_crc8_starts(const hcrc_crc8_t *config)
+{
+    if(config==NULL)
+    {
+        config=&hcrc_crc8_default;
+    }
+    return config->init;
+}
+
 /*
  * 此函数可优化
  */
@@ -66,49 +75,6 @@ static uint8_t hcrc_crc8_table_reversal(const hcrc_crc8_t *config,uint8_t index,
         }
     }
     return crc;
-}
-
-/*
- * 当输入与输出均反转时，采用反转表达式的做法
- */
-static uint8_t hcrc_crc8_calculate_reversal(const hcrc_crc8_t *config,const uint8_t *data,size_t datalen)
-{
-    if(config==NULL)
-    {
-        return 0;
-    }
-    uint8_t crc=config->init;
-    uint8_t poly=config->poly;
-    {
-        uint8_t temp=0;
-        for(size_t i=0; i < sizeof(poly)*8 ; i++)
-        {
-            if(poly & (1U << (i)))
-            {
-                temp |= (1U << (sizeof(poly)*8-1-i));
-            }
-        }
-        poly=temp;
-    }
-    if(data!=NULL && datalen > 0)
-    {
-        for(size_t i=0; i<datalen; i++)
-        {
-            uint8_t current_data=data[i];
-
-            crc = hcrc_crc8_table_reversal(config,current_data^crc,poly);
-        }
-    }
-
-
-
-    /*
-     * 异或输出
-     */
-    crc ^= config->xorout;
-
-    return crc;
-
 }
 
 /*
@@ -135,67 +101,108 @@ static uint8_t hcrc_crc8_table(const hcrc_crc8_t *config,uint8_t index)
     return crc;
 }
 
-uint8_t hcrc_crc8_calculate(const hcrc_crc8_t *config,const uint8_t *data,size_t datalen)
+hcrc_crc8_context_t hcrc_crc8_update(const hcrc_crc8_t *config,hcrc_crc8_context_t ctx,const uint8_t *data,size_t datalen)
 {
     if(config==NULL)
     {
         config=&hcrc_crc8_default;
     }
+    uint8_t crc=ctx;
     if(config->refin && config->refout)
     {
-        /*
-         * 使用替换的算法
-         */
-        return hcrc_crc8_calculate_reversal(config,data,datalen);
-    }
-    uint8_t crc=config->init;
-    if(data!=NULL && datalen > 0)
-    {
-        for(size_t i=0; i<datalen; i++)
+        uint8_t poly=config->poly;
         {
-            uint8_t current_data=data[i];
-            /*
-             * 输入数据反转
-             */
-            if(config->refin)
+            uint8_t temp=0;
+            for(size_t i=0; i < sizeof(poly)*8 ; i++)
             {
-                uint8_t temp=0;
-                for(size_t i=0; i < sizeof(current_data)*8 ; i++)
+                if(poly & (1U << (i)))
                 {
-                    if(current_data & (1U << (i)))
-                    {
-                        temp |= (1U << (sizeof(current_data)*8-1-i));
-                    }
+                    temp |= (1U << (sizeof(poly)*8-1-i));
                 }
-                current_data=temp;
             }
-
-            crc = hcrc_crc8_table(config,current_data^crc);
+            poly=temp;
         }
-    }
-
-    /*
-     * 输出结果反转
-     */
-    if(config->refout)
-    {
-        uint8_t temp=0;
-        for(size_t i=0; i < sizeof(crc)*8 ; i++)
+        if(data!=NULL && datalen > 0)
         {
-            if(crc & (1U << (i)))
+            for(size_t i=0; i<datalen; i++)
             {
-                temp |= (1U << (sizeof(crc)*8-1-i));
+                uint8_t current_data=data[i];
+
+                crc = hcrc_crc8_table_reversal(config,current_data^crc,poly);
             }
         }
-        crc=temp;
+
+    }
+    else
+    {
+        if(data!=NULL && datalen > 0)
+        {
+            for(size_t i=0; i<datalen; i++)
+            {
+                uint8_t current_data=data[i];
+                /*
+                 * 输入数据反转
+                 */
+                if(config->refin)
+                {
+                    uint8_t temp=0;
+                    for(size_t i=0; i < sizeof(current_data)*8 ; i++)
+                    {
+                        if(current_data & (1U << (i)))
+                        {
+                            temp |= (1U << (sizeof(current_data)*8-1-i));
+                        }
+                    }
+                    current_data=temp;
+                }
+
+                crc = hcrc_crc8_table(config,current_data^crc);
+            }
+        }
+    }
+
+    return crc;
+}
+
+uint8_t hcrc_crc8_finish(const hcrc_crc8_t *config,hcrc_crc8_context_t ctx)
+{
+    if(config==NULL)
+    {
+        config=&hcrc_crc8_default;
+    }
+    uint8_t crc=ctx;
+    if(!(config->refin && config->refout))
+    {
+        /*
+         * 输出结果反转
+         */
+        if(config->refout)
+        {
+            uint8_t temp=0;
+            for(size_t i=0; i < sizeof(crc)*8 ; i++)
+            {
+                if(crc & (1U << (i)))
+                {
+                    temp |= (1U << (sizeof(crc)*8-1-i));
+                }
+            }
+            crc=temp;
+        }
     }
 
     /*
-     * 异或输出
-     */
+    * 异或输出
+    */
     crc ^= config->xorout;
 
     return crc;
+}
+
+uint8_t hcrc_crc8_calculate(const hcrc_crc8_t *config,const uint8_t *data,size_t datalen)
+{
+    hcrc_crc8_context_t ctx=hcrc_crc8_starts(config);
+    ctx=hcrc_crc8_update(config,ctx,data,datalen);
+    return hcrc_crc8_finish(config,ctx);
 }
 
 bool hcrc_crc8_check(const hcrc_crc8_t *config,const uint8_t *data,size_t datalen,uint8_t check)
@@ -276,136 +283,144 @@ const hcrc_crc16_t  hcrc_crc16_dnp=
     true
 };
 
-/*
- * 当输入与输出均反转时，采用反转表达式的做法
- */
-static uint16_t hcrc_crc16_calculate_reversal(const hcrc_crc16_t *config,const uint8_t *data,size_t datalen)
-{
-    if(config==NULL)
-    {
-        return 0;
-    }
-    uint16_t crc=config->init;
-    /*
-     * 反转表达式
-     */
-    uint16_t poly=config->poly;
-    {
-        uint16_t temp=0;
-        for(size_t i=0; i < sizeof(poly)*8 ; i++)
-        {
-            if(poly & (1U << (i)))
-            {
-                temp |= (1U << (sizeof(poly)*8-1-i));
-            }
-        }
-        poly=temp;
-    }
-    if(data!=NULL && datalen > 0)
-    {
-        for(size_t i=0; i<datalen; i++)
-        {
-            uint8_t current_data=data[i];
-            crc ^= (((uint16_t)current_data));
-
-            for(size_t j=0; j<8; j++)
-            {
-                if((crc & 1) > 0)
-                {
-                    crc = ((crc >> 1) ^ poly);
-                }
-                else
-                {
-                    crc = (crc >> 1);
-                }
-            }
-        }
-    }
-
-
-    /*
-     * 异或输出
-     */
-    crc ^= config->xorout;
-
-    return crc;
-
-}
-
-uint16_t hcrc_crc16_calculate(const hcrc_crc16_t *config,const uint8_t *data,size_t datalen)
+hcrc_crc16_context_t hcrc_crc16_starts(const hcrc_crc16_t *config)
 {
     if(config==NULL)
     {
         config=&hcrc_crc16_modbus;
     }
+    return config->init;
+}
+
+
+hcrc_crc16_context_t hcrc_crc16_update(const hcrc_crc16_t *config,hcrc_crc16_context_t ctx,const uint8_t *data,size_t datalen)
+{
+    if(config==NULL)
+    {
+        config=&hcrc_crc16_modbus;
+    }
+    uint16_t crc=ctx;
     if(config->refin && config->refout)
     {
         /*
-         * 使用替换的算法
-         */
-        return  hcrc_crc16_calculate_reversal(config,data,datalen);
-    }
-    uint16_t crc=config->init;
-    if(data!=NULL && datalen > 0)
-    {
-        for(size_t i=0; i<datalen; i++)
+        * 反转表达式
+        */
+        uint16_t poly=config->poly;
         {
-            uint8_t current_data=data[i];
-            /*
-             * 输入数据反转
-             */
-            if(config->refin)
+            uint16_t temp=0;
+            for(size_t i=0; i < sizeof(poly)*8 ; i++)
             {
-                uint8_t temp=0;
-                for(size_t i=0; i < sizeof(current_data)*8 ; i++)
+                if(poly & (1U << (i)))
                 {
-                    if(current_data & (1U << (i)))
+                    temp |= (1U << (sizeof(poly)*8-1-i));
+                }
+            }
+            poly=temp;
+        }
+        if(data!=NULL && datalen > 0)
+        {
+            for(size_t i=0; i<datalen; i++)
+            {
+                uint8_t current_data=data[i];
+                crc ^= (((uint16_t)current_data));
+
+                for(size_t j=0; j<8; j++)
+                {
+                    if((crc & 1) > 0)
                     {
-                        temp |= (1U << (sizeof(current_data)*8-1-i));
+                        crc = ((crc >> 1) ^ poly);
+                    }
+                    else
+                    {
+                        crc = (crc >> 1);
                     }
                 }
-                current_data=temp;
-            }
-
-            crc ^= (((uint16_t)current_data) <<8);
-
-            for(size_t j=0; j<8; j++)
-            {
-                if((crc & 0x8000) > 0)
-                {
-                    crc = ((crc << 1) ^ config->poly);
-                }
-                else
-                {
-                    crc = (crc << 1);
-                }
             }
         }
-    }
 
-    /*
-     * 输出结果反转
-     */
-    if(config->refout)
+    }
+    else
     {
-        uint16_t temp=0;
-        for(size_t i=0; i < sizeof(crc)*8 ; i++)
+        if(data!=NULL && datalen > 0)
         {
-            if(crc & (1U << (i)))
+            for(size_t i=0; i<datalen; i++)
             {
-                temp |= (1U << (sizeof(crc)*8-1-i));
+                uint8_t current_data=data[i];
+                /*
+                 * 输入数据反转
+                 */
+                if(config->refin)
+                {
+                    uint8_t temp=0;
+                    for(size_t i=0; i < sizeof(current_data)*8 ; i++)
+                    {
+                        if(current_data & (1U << (i)))
+                        {
+                            temp |= (1U << (sizeof(current_data)*8-1-i));
+                        }
+                    }
+                    current_data=temp;
+                }
+
+                crc ^= (((uint16_t)current_data) <<8);
+
+                for(size_t j=0; j<8; j++)
+                {
+                    if((crc & 0x8000) > 0)
+                    {
+                        crc = ((crc << 1) ^ config->poly);
+                    }
+                    else
+                    {
+                        crc = (crc << 1);
+                    }
+                }
             }
         }
-        crc=temp;
+    }
+
+    return crc;
+}
+
+uint16_t hcrc_crc16_finish(const hcrc_crc16_t *config,hcrc_crc16_context_t ctx)
+{
+    if(config==NULL)
+    {
+        config=&hcrc_crc16_modbus;
+    }
+    uint16_t crc=ctx;
+    if(!(config->refin && config->refout))
+    {
+        /*
+         * 输出结果反转
+         */
+        if(config->refout)
+        {
+            uint8_t temp=0;
+            for(size_t i=0; i < sizeof(crc)*8 ; i++)
+            {
+                if(crc & (1U << (i)))
+                {
+                    temp |= (1U << (sizeof(crc)*8-1-i));
+                }
+            }
+            crc=temp;
+        }
     }
 
     /*
-     * 异或输出
-     */
+    * 异或输出
+    */
     crc ^= config->xorout;
 
     return crc;
+}
 
+uint16_t hcrc_crc16_calculate(const hcrc_crc16_t *config,const uint8_t *data,size_t datalen)
+{
+    hcrc_crc16_context_t ctx=hcrc_crc16_starts(config);
+    ctx=hcrc_crc16_update(config,ctx,data,datalen);
+    return hcrc_crc16_finish(config,ctx);
 }
 
 bool hcrc_crc16_check(const hcrc_crc16_t *config,const uint8_t *data,size_t datalen,uint16_t check)
@@ -432,141 +447,147 @@ const hcrc_crc32_t hcrc_crc32_mpeg_2=
     false
 };
 
-/*
- * 当输入与输出均反转时，采用反转表达式的做法
- */
-static uint32_t hcrc_crc32_calculate_reversal(const hcrc_crc32_t *config,const uint8_t *data,size_t datalen)
-{
-    if(config == NULL)
-    {
-        return 0;
-    }
-    uint32_t crc=config->init;
-    uint32_t poly=config->poly;
-    /*
-     * 反转表达式
-     */
-    {
-        uint32_t temp=0;
-        for(size_t i=0; i < sizeof(poly)*8 ; i++)
-        {
-            if(poly & (1U << (i)))
-            {
-                temp |= (1U << (sizeof(poly)*8-1-i));
-            }
-        }
-        poly=temp;
-    }
-
-    if(data!=NULL && datalen > 0)
-    {
-        for(size_t i=0; i<datalen; i++)
-        {
-            uint8_t current_data=data[i];
-
-            crc ^= ((uint32_t)current_data);
-
-            for(size_t j=0; j<8; j++)
-            {
-                if((crc & 1) > 0)
-                {
-                    crc = ((crc >> 1) ^ poly);
-                }
-                else
-                {
-                    crc = (crc >> 1);
-                }
-            }
-        }
-    }
-
-
-
-    /*
-     * 异或输出
-     */
-    crc ^= config->xorout;
-
-    return crc;
-
-}
-
-uint32_t hcrc_crc32_calculate(const hcrc_crc32_t *config,const uint8_t *data,size_t datalen)
+hcrc_crc32_context_t hcrc_crc32_starts(const hcrc_crc32_t *config)
 {
     if(config==NULL)
     {
         config=&hcrc_crc32_default;
     }
+    return config->init;
+}
 
-    if(config->refin && config ->refout)
+
+hcrc_crc32_context_t hcrc_crc32_update(const hcrc_crc32_t *config,hcrc_crc32_context_t ctx,const uint8_t *data,size_t datalen)
+{
+    if(config==NULL)
     {
-        /*
-         * 使用替换的算法
-         */
-        return hcrc_crc32_calculate_reversal(config,data,datalen);
+        config=&hcrc_crc32_default;
     }
-
-    uint32_t crc=config->init;
-    if(data!=NULL && datalen > 0)
+    uint32_t crc=ctx;
+    if(config->refin && config->refout)
     {
-        for(size_t i=0; i<datalen; i++)
+        uint32_t poly=config->poly;
+        /*
+         * 反转表达式
+         */
         {
-            uint8_t current_data=data[i];
-            /*
-             * 输入数据反转
-             */
-            if(config->refin)
+            uint32_t temp=0;
+            for(size_t i=0; i < sizeof(poly)*8 ; i++)
             {
-                uint8_t temp=0;
-                for(size_t i=0; i < sizeof(current_data)*8 ; i++)
+                if(poly & (1U << (i)))
                 {
-                    if(current_data & (1U << (i)))
+                    temp |= (1U << (sizeof(poly)*8-1-i));
+                }
+            }
+            poly=temp;
+        }
+
+        if(data!=NULL && datalen > 0)
+        {
+            for(size_t i=0; i<datalen; i++)
+            {
+                uint8_t current_data=data[i];
+
+                crc ^= ((uint32_t)current_data);
+
+                for(size_t j=0; j<8; j++)
+                {
+                    if((crc & 1) > 0)
                     {
-                        temp |= (1U << (sizeof(current_data)*8-1-i));
+                        crc = ((crc >> 1) ^ poly);
+                    }
+                    else
+                    {
+                        crc = (crc >> 1);
                     }
                 }
-                current_data=temp;
-            }
-
-            crc ^= (((uint32_t)current_data) <<24);
-
-            for(size_t j=0; j<8; j++)
-            {
-                if((crc & 0x80000000) > 0)
-                {
-                    crc = ((crc << 1) ^ config->poly);
-                }
-                else
-                {
-                    crc = (crc << 1);
-                }
             }
         }
-    }
 
-    /*
-     * 输出结果反转
-     */
-    if(config->refout)
+    }
+    else
     {
-        uint32_t temp=0;
-        for(size_t i=0; i < sizeof(crc)*8 ; i++)
+        if(data!=NULL && datalen > 0)
         {
-            if(crc & (1U << (i)))
+            for(size_t i=0; i<datalen; i++)
             {
-                temp |= (1U << (sizeof(crc)*8-1-i));
+                uint8_t current_data=data[i];
+                /*
+                 * 输入数据反转
+                 */
+                if(config->refin)
+                {
+                    uint8_t temp=0;
+                    for(size_t i=0; i < sizeof(current_data)*8 ; i++)
+                    {
+                        if(current_data & (1U << (i)))
+                        {
+                            temp |= (1U << (sizeof(current_data)*8-1-i));
+                        }
+                    }
+                    current_data=temp;
+                }
+
+                crc ^= (((uint32_t)current_data) <<24);
+
+                for(size_t j=0; j<8; j++)
+                {
+                    if((crc & 0x80000000) > 0)
+                    {
+                        crc = ((crc << 1) ^ config->poly);
+                    }
+                    else
+                    {
+                        crc = (crc << 1);
+                    }
+                }
             }
         }
-        crc=temp;
+
+    }
+
+    return crc;
+}
+
+uint32_t hcrc_crc32_finish(const hcrc_crc32_t *config,hcrc_crc32_context_t ctx)
+{
+    if(config==NULL)
+    {
+        config=&hcrc_crc32_default;
+    }
+    uint32_t crc=ctx;
+    if(!(config->refin && config->refout))
+    {
+        /*
+         * 输出结果反转
+         */
+        if(config->refout)
+        {
+            uint8_t temp=0;
+            for(size_t i=0; i < sizeof(crc)*8 ; i++)
+            {
+                if(crc & (1U << (i)))
+                {
+                    temp |= (1U << (sizeof(crc)*8-1-i));
+                }
+            }
+            crc=temp;
+        }
     }
 
     /*
-     * 异或输出
-     */
+    * 异或输出
+    */
     crc ^= config->xorout;
 
     return crc;
+}
 
+uint32_t hcrc_crc32_calculate(const hcrc_crc32_t *config,const uint8_t *data,size_t datalen)
+{
+    hcrc_crc32_context_t ctx=hcrc_crc32_starts(config);
+    ctx=hcrc_crc32_update(config,ctx,data,datalen);
+    return hcrc_crc32_finish(config,ctx);
 }
 
 bool hcrc_crc32_check(const hcrc_crc32_t *config,const uint8_t *data,size_t datalen,uint32_t check)
--- hbox/hcrypto/hcrc.h
+++ hbox/hcrypto/hcrc.h
@@ -33,6 +33,39 @@ extern const hcrc_crc8_t  hcrc_crc8_itu;        /**< CRC8/ITU */
 extern const hcrc_crc8_t  hcrc_crc8_rohc;       /**< CRC8/ROHC */
 extern const hcrc_crc8_t  hcrc_crc8_maxim;      /**< CRC8/MAXIM */
 
+
+typedef uint8_t hcrc_crc8_context_t;        /**< crc8上下文 */
+
+/** \brief CRC8开始
+ *
+ * \param config const hcrc_crc8_t* CRC8配置（注意:开始、更新、完成需要使用同一个配置）
+ * \return hcrc_crc8_context_t CRC8上下文
+ *
+ */
+hcrc_crc8_context_t hcrc_crc8_starts(const hcrc_crc8_t *config);
+
+
+/** \brief CRC8更新
+ *
+ * \param config const hcrc_crc8_t* CRC8配置（注意:开始、更新、完成需要使用同一个配置）
+ * \param ctx hcrc_crc8_context_t  CRC8上下文
+ * \param data const uint8_t* 数据
+ * \param datalen size_t 数据长度
+ * \return hcrc_crc8_context_t 更新后的上下文
+ *
+ */
+hcrc_crc8_context_t hcrc_crc8_update(const hcrc_crc8_t *config,hcrc_crc8_context_t ctx,const uint8_t *data,size_t datalen);
+
+
+/** \brief CRC8完成
+ *
+ * \param config const hcrc_crc8_t* CRC8配置（注意:开始、更新、完成需要使用同一个配置）
+ * \param ctx hcrc_crc8_context_t CRC8上下文
+ * \return uint8_t CRC8
+ *
+ */
+uint8_t hcrc_crc8_finish(const hcrc_crc8_t *config,hcrc_crc8_context_t ctx);
+
 /** \brief crc8 计算
  *
  * \param config const hcrc_crc8_t * CRC8配置
@@ -93,6 +126,38 @@ extern const hcrc_crc16_t  hcrc_crc16_x25;          /**< CRC16/X25 */
 extern const hcrc_crc16_t  hcrc_crc16_xmodem;       /**< CRC16/XMODEM */
 extern const hcrc_crc16_t  hcrc_crc16_dnp;          /**< CRC16/DNP */
 
+typedef uint16_t hcrc_crc16_context_t;        /**< crc16上下文 */
+
+/** \brief CRC16开始
+ *
+ * \param config const hcrc_crc16_t* CRC16配置（注意:开始、更新、完成需要使用同一个配置）
+ * \return hcrc_crc16_context_t CRC16上下文
+ *
+ */
+hcrc_crc16_context_t hcrc_crc16_starts(const hcrc_crc16_t *config);
+
+
+/** \brief CRC16更新
+ *
+ * \param config const hcrc_crc16_t* CRC16配置（注意:开始、更新、完成需要使用同一个配置）
+ * \param ctx hcrc_crc16_context_t  CRC16上下文
+ * \param data const uint16_t* 数据
+ * \param datalen size_t 数据长度
+ * \return hcrc_crc16_context_t 更新后的上下文
+ *
+ */
+hcrc_crc16_context_t hcrc_crc16_update(const hcrc_crc16_t *config,hcrc_crc16_context_t ctx,const uint8_t *data,size_t datalen);
+
+
+/** \brief CRC16完成
+ *
+ * \param config const hcrc_crc16_t* CRC16配置（注意:开始、更新、完成需要使用同一个配置）
+ * \param ctx hcrc_crc16_context_t CRC16上下文
+ * \return uint16_t CRC16
+ *
+ */
+uint16_t hcrc_crc16_finish(const hcrc_crc16_t *config,hcrc_crc16_context_t ctx);
+
 /** \brief crc16 计算
  *
  * \param config const hcrc_crc16_t * CRC16配置
@@ -146,6 +211,38 @@ struct hcrc_crc32
 
 extern const hcrc_crc32_t hcrc_crc32_default; /**< 默认CRC32校验 */
 extern const hcrc_crc32_t hcrc_crc32_mpeg_2;  /**< CRC32/MPEG-2 */
+
+typedef uint32_t hcrc_crc32_context_t;        /**< crc32上下文 */
+
+/** \brief CRC32开始
+ *
+ * \param config const hcrc_crc32_t* CRC32配置（注意:开始、更新、完成需要使用同一个配置）
+ * \return hcrc_crc32_context_t CRC32上下文
+ *
+ */
+hcrc_crc32_context_t hcrc_crc32_starts(const hcrc_crc32_t *config);
+
+
+/** \brief CRC32更新
+ *
+ * \param config const hcrc_crc32_t* CRC32配置（注意:开始、更新、完成需要使用同一个配置）
+ * \param ctx hcrc_crc32_context_t  CRC32上下文
+ * \param data const uint32_t* 数据
+ * \param datalen size_t 数据长度
+ * \return hcrc_crc32_context_t 更新后的上下文
+ *
+ */
+hcrc_crc32_context_t hcrc_crc32_update(const hcrc_crc32_t *config,hcrc_crc32_context_t ctx,const uint8_t *data,size_t datalen);
+
+
+/** \brief CRC32完成
+ *
+ * \param config const hcrc_crc32_t* CRC32配置（注意:开始、更新、完成需要使用同一个配置）
+ * \param ctx hcrc_crc32_context_t CRC32上下文
+ * \return uint32_t CRC32
+ *
+ */
+uint32_t hcrc_crc32_finish(const hcrc_crc32_t *config,hcrc_crc32_context_t ctx);
 
 /** \brief crc32 计算
  *
