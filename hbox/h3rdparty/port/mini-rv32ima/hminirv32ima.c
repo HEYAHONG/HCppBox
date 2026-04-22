d88a05ef5e741de49c638d352f860fe80d523b7a
HEYAHONG <2229388563@qq.com>
2026-04-22 15:34:59 +0800

    [htools] 初步添加hmini-rv32imav2

--- hbox/h3rdparty/port/mini-rv32ima/hminirv32ima.c
+++ hbox/h3rdparty/port/mini-rv32ima/hminirv32ima.c
@@ -7,6 +7,7 @@
  * License:   MIT
  **************************************************************/
 #include "hdefaults.h"
+#include "hmemory.h"
 #include "hminirv32ima.h"
 
 static void hminirv32ima_state_csr_write(struct hminirv32ima_state *state,uint32_t csrno,uint32_t csrvalue)
@@ -81,7 +82,8 @@ void hminirv32ima_core_init(hminirv32ima_core_t *core,size_t ram_size)
         core->memory.ram_size=ram_size;
         core->memory.mem_load=hminirv32ima_memory_load;
         core->memory.mem_store=hminirv32ima_memory_store;
-        core->state.regs[2]=core->memory.ram_base+core->memory.ram_size;/**< 初始化SP指针 */
+        core->state.pc = core->memory.ram_base;                                 /**< 设置PC值 */
+        core->state.regs[2]=core->memory.ram_base+core->memory.ram_size;        /**< 初始化SP指针 */
 
         /*
          * 初始化MMIO
@@ -91,6 +93,365 @@ void hminirv32ima_core_init(hminirv32ima_core_t *core,size_t ram_size)
         core->mmio.mmio_load=hminirv32ima_mmio_load;
         core->mmio.mmio_store=hminirv32ima_mmio_store;
     }
+}
+
+#include H3RDPARTY_MINI_RV32IMA_64MB_DTB_HEADER
+
+static size_t hminirv32ima_machine_default64mb_memory_load(const struct hminirv32ima_memory *mem,uintptr_t ram_addr,void *ptr,size_t length)
+{
+    const hminirv32ima_core_t                   *   core=GET_STRUCT_PTR_BY_MEMBER_PTR(mem,hminirv32ima_core_t,memory);
+    const hminirv32ima_machine_default64mb_t    *   machine=GET_STRUCT_PTR_BY_MEMBER_PTR(core,hminirv32ima_machine_default64mb_t,core);
+    if(ram_addr >= mem->ram_base && ram_addr < (mem->ram_base+mem->ram_size) && (ram_addr+length) <= (mem->ram_base+mem->ram_size))
+    {
+        size_t ofs=ram_addr-mem->ram_base;
+        switch(length)
+        {
+        case 1:
+        {
+            uint8_t * data=(uint8_t *)ptr;
+            data[0]=machine->ram[ofs];
+        }
+        break;
+        case 2:
+        {
+            uint16_t * data=(uint16_t *)ptr;
+            (*data)=machine->ram[ofs+1];
+            (*data) <<= 8;
+            (*data)+=machine->ram[ofs];
+        }
+        break;
+        case 4:
+        {
+            uint32_t * data=(uint32_t *)ptr;
+            (*data)=machine->ram[ofs+3];
+            (*data) <<= 8;
+            (*data)+=machine->ram[ofs+2];
+            (*data) <<= 8;
+            (*data)+=machine->ram[ofs+1];
+            (*data) <<= 8;
+            (*data)+=machine->ram[ofs];
+        }
+        break;
+        }
+        return length;
+    }
+    return 0;
+}
+
+static size_t hminirv32ima_machine_default64mb_memory_store(const struct hminirv32ima_memory *mem,uintptr_t ram_addr,const void *ptr,size_t length)
+{
+    const hminirv32ima_core_t                   *   core=GET_STRUCT_PTR_BY_MEMBER_PTR(mem,hminirv32ima_core_t,memory);
+    hminirv32ima_machine_default64mb_t          *   machine=(hminirv32ima_machine_default64mb_t *)GET_STRUCT_PTR_BY_MEMBER_PTR(core,hminirv32ima_machine_default64mb_t,core);
+    if(ram_addr >= mem->ram_base && ram_addr < (mem->ram_base+mem->ram_size) && (ram_addr+length) <= (mem->ram_base+mem->ram_size))
+    {
+        size_t ofs=ram_addr-mem->ram_base;
+        switch(length)
+        {
+        case 1:
+        {
+            const uint8_t * data=(const uint8_t *)ptr;
+            machine->ram[ofs]=data[0];
+        }
+        break;
+        case 2:
+        {
+            const uint16_t * data=(const uint16_t *)ptr;
+            machine->ram[ofs]=((*data)&0xFF);
+            machine->ram[ofs+1]=(((*data)>>8)&0xFF);
+        }
+        break;
+        case 4:
+        {
+            const uint32_t *data=(const uint32_t *)ptr;
+            machine->ram[ofs]=((*data)&0xFF);
+            machine->ram[ofs+1]=(((*data)>>8)&0xFF);
+            machine->ram[ofs+2]=(((*data)>>16)&0xFF);
+            machine->ram[ofs+3]=(((*data)>>24)&0xFF);
+        }
+        break;
+        }
+        return length;
+    }
+    return 0;
+}
+
+void hminirv32ima_machine_default64mb_init(hminirv32ima_machine_default64mb_t *machine)
+{
+    if(machine!=NULL)
+    {
+        /*
+         * 复位机器
+         */
+        hminirv32ima_machine_default64mb_reset(machine);
+
+        /*
+         * 清空内存
+         */
+        memset(machine->ram,0,sizeof(machine->ram));
+
+        /*
+         * 设置回调
+         */
+        machine->console_has_data=NULL;
+        machine->console_put_data=NULL;
+        machine->console_get_data=NULL;
+
+    }
+}
+
+static size_t hminirv32ima_machine_default64mb_mmio_load(const struct hminirv32ima_mmio *mmio,uintptr_t mmio_addr,void *ptr,size_t length)
+{
+    const hminirv32ima_core_t                   *   core=GET_STRUCT_PTR_BY_MEMBER_PTR(mmio,hminirv32ima_core_t,mmio);
+    const hminirv32ima_machine_default64mb_t    *   machine=GET_STRUCT_PTR_BY_MEMBER_PTR(core,hminirv32ima_machine_default64mb_t,core);
+    if(length==4)
+    {
+        uint32_t *data=(uint32_t *)ptr;
+        if(mmio_addr>=0x10000000 && mmio_addr < (0x10000000+0x100))
+        {
+            /*
+             * 8250串口
+             */
+            size_t offset=mmio_addr-0x10000000;
+            uint8_t reg_value=0;
+            hs_common_serial_8250_bus_read((hs_common_serial_8250_t *)&machine->serial8250,offset,&reg_value);
+            (*data)=reg_value;
+        }
+        else if(mmio_addr>=0x11000000 && mmio_addr < (0x11000000+0x10000))
+        {
+            /*
+             * clint
+             */
+            size_t offset=mmio_addr-0x11000000;
+            switch(offset)
+            {
+            case 0xBFF8:
+            {
+                (*data)=machine->core.state.timerl;
+            }
+            break;
+            case 0xBFFC:
+            {
+                (*data)=machine->core.state.timerh;
+            }
+            break;
+            default:
+            {
+
+            }
+            break;
+            }
+
+        }
+        return length;
+    }
+    return 0;
+}
+
+static size_t hminirv32ima_machine_default64mb_mmio_store(const struct hminirv32ima_mmio *mmio,uintptr_t mmio_addr,const void *ptr,size_t length)
+{
+    const hminirv32ima_core_t                   *   core=GET_STRUCT_PTR_BY_MEMBER_PTR(mmio,hminirv32ima_core_t,mmio);
+    hminirv32ima_machine_default64mb_t          *   machine=(hminirv32ima_machine_default64mb_t *)GET_STRUCT_PTR_BY_MEMBER_PTR(core,hminirv32ima_machine_default64mb_t,core);
+    if(length==4)
+    {
+        const uint32_t *data=(const uint32_t *)ptr;
+        if(mmio_addr>=0x10000000 && mmio_addr < (0x10000000+0x100))
+        {
+            /*
+             * 8250串口
+             */
+            size_t offset=mmio_addr-0x10000000;
+            hs_common_serial_8250_bus_write(&machine->serial8250,offset,(*data));
+
+        }
+        else if(mmio_addr>=0x11000000 && mmio_addr < (0x11000000+0x10000))
+        {
+            /*
+             * clint
+             */
+            size_t offset=mmio_addr-0x11000000;
+            switch(offset)
+            {
+            case 0x4000:
+            {
+                machine->core.state.timermatchl=(*data);
+            }
+            break;
+            case 0x4004:
+            {
+                machine->core.state.timermatchh=(*data);
+            }
+            break;
+            default:
+            {
+
+            }
+            break;
+            }
+
+        }
+        else if(mmio_addr>= 0x11100000 && mmio_addr < (0x11100000+0x1000))
+        {
+            /*
+             * syscon
+             */
+            size_t offset=mmio_addr-0x11100000;
+            switch(offset)
+            {
+            case 0:
+            {
+                machine->syscon=(*data);
+            }
+            break;
+            default:
+            {
+
+            }
+            break;
+            }
+
+        }
+        return length;
+    }
+    return 0;
+}
+
+static bool hminirv32ima_machine_default64mb_serial_8250_io_callback(struct hs_common_serial_8250 *dev,hs_common_serial_8250_io_operate_t io_operate,uint8_t *data)
+{
+    if(dev==NULL)
+    {
+        return false;
+    }
+    bool ret=false;
+    const hminirv32ima_machine_default64mb_t    *   machine=GET_STRUCT_PTR_BY_MEMBER_PTR(dev,hminirv32ima_machine_default64mb_t,serial8250);
+    switch(io_operate)
+    {
+    case HS_COMMON_SERIAL_8250_IO_OPERATE_TICK:
+    {
+        /*
+         * 更新LSR寄存器
+         */
+        dev->registers[HS_COMMON_SERIAL_8250_REGISTER_LSR]=0x60;
+
+        if(machine->console_has_data!=NULL)
+        {
+            dev->registers[HS_COMMON_SERIAL_8250_REGISTER_LSR] |=  machine->console_has_data(machine)?0x01:0x00;
+        }
+        ret=true;
+    }
+    break;
+    case HS_COMMON_SERIAL_8250_IO_OPERATE_TRANSMIT_BYTE:
+    {
+        if(machine->console_put_data!=NULL)
+        {
+            machine->console_put_data(machine,(*data));
+        }
+        ret=true;
+    }
+    break;
+    case HS_COMMON_SERIAL_8250_IO_OPERATE_RECEIVE_BYTE:
+    {
+        if(machine->console_get_data!=NULL)
+        {
+            (*data)=machine->console_get_data(machine);
+        }
+        ret=true;
+    }
+    break;
+    default:
+    {
+
+    }
+    break;
+    }
+    return ret;
+}
+
+void hminirv32ima_machine_default64mb_reset(hminirv32ima_machine_default64mb_t *machine)
+{
+    if(machine!=NULL)
+    {
+        /*
+         * 初始化core
+         */
+        hminirv32ima_core_init(&machine->core,sizeof(machine->ram)+sizeof(machine->dtb));
+
+        /*
+         * 初始化dtb
+         */
+        memset(machine->dtb,0,sizeof(machine->dtb));
+        memcpy(machine->dtb,default64mbdtb,sizeof(machine->dtb)>sizeof(default64mbdtb)?sizeof(default64mbdtb):sizeof(machine->dtb));
+
+        /*
+         * 设置内存信息
+         */
+        machine->core.memory.mem_load=hminirv32ima_machine_default64mb_memory_load;
+        machine->core.memory.mem_store=hminirv32ima_machine_default64mb_memory_store;
+
+        /*
+         * 设置MMIO
+         */
+        machine->core.mmio.mmio_load=hminirv32ima_machine_default64mb_mmio_load;
+        machine->core.mmio.mmio_store=hminirv32ima_machine_default64mb_mmio_store;
+
+        /*
+         * serial 8250
+         */
+        hs_common_serial_8250_init(&machine->serial8250,hminirv32ima_machine_default64mb_serial_8250_io_callback,NULL,HS_COMMON_SERIAL_8250_CLK_FREQ);
+
+        /*
+         * syscon
+         */
+        machine->syscon=0;
+
+
+        /*
+         * 初始化寄存器
+         */
+        machine->core.state.pc = machine->core.memory.ram_base;                                 /**< 初始化PC值 */
+        machine->core.state.regs[2]=machine->core.memory.ram_base+sizeof(machine->ram);         /**< 初始化SP指针 */
+        machine->core.state.regs[10]=0;                                                         /**< 初始化A0（HART地址） */
+        machine->core.state.regs[11]=machine->core.memory.ram_base+sizeof(machine->ram);        /**< 初始化A1(DTS地址) */
+    }
+}
+
+bool hminirv32ima_machine_default64mb_load_image(hminirv32ima_machine_default64mb_t *machine,const uint8_t *image,size_t image_size)
+{
+    if(machine!=NULL && image != NULL && image_size < sizeof(machine->ram))
+    {
+        memcpy(machine->ram,image,image_size);
+        return true;
+    }
+    return false;
+}
+
+bool hminirv32ima_machine_default64mb_load_dtb(hminirv32ima_machine_default64mb_t *machine,const uint8_t *dtb,size_t dtb_size)
+{
+    if(machine!=NULL && dtb != NULL && dtb_size < sizeof(machine->dtb))
+    {
+        memcpy(machine->dtb,dtb,dtb_size);
+        return true;
+    }
+    return false;
+}
+
+int hminirv32ima_machine_default64mb_step(hminirv32ima_machine_default64mb_t *machine,uint32_t elapsedUs, int insn_count )
+{
+    if(machine==NULL)
+    {
+        return -1;
+    }
+
+    if(machine->syscon==0x5555 || machine->syscon==0x7777)
+    {
+        /*
+         * 关机与重启直接返回
+         */
+        return machine->syscon;
+    }
+
+    hs_common_serial_8250_bus_tick(&machine->serial8250);
+
+    return hminirv32ima_core_step(&machine->core,elapsedUs,insn_count);
 }
 
 #define MINIRV32_RAM_IMAGE_OFFSET                      (core->memory.ram_base)
@@ -173,3 +534,5 @@ int hminirv32ima_core_step(hminirv32ima_core_t *core,uint32_t elapsedUs, int ins
     }
     return hminirv32ima_step_internal(core,0,elapsedUs,insn_count);
 }
+
+
--- hbox/h3rdparty/port/mini-rv32ima/hminirv32ima.h
+++ hbox/h3rdparty/port/mini-rv32ima/hminirv32ima.h
@@ -12,6 +12,7 @@
 #include "stdbool.h"
 #include "stdint.h"
 #include "stdlib.h"
+#include "hsimulator.h"
 #ifdef __cplusplus
 extern "C"
 {
@@ -152,12 +153,97 @@ void hminirv32ima_core_init(hminirv32ima_core_t *core,size_t ram_size);
 /** \brief mini-rv32ima内核步进
  *
  * \param core hminirv32ima_core_t* mini-rv32ima内核，注意：一定要初始化完成且设置回掉后使用
- * \param elapsedUs uint32_t 上一步到本步间隔的秒数，用于维护RISCV内部的定时器
+ * \param elapsedUs uint32_t 上一步到本步间隔的微秒数，用于维护RISCV内部的定时器
  * \param count int 执行指令的条数
  * \return int 错误代码，0=正常运行，1=WFI指令,-1=参数错误
  *
  */
 int hminirv32ima_core_step(hminirv32ima_core_t *core,uint32_t elapsedUs, int insn_count );
+
+
+/*
+ * 默认机器:
+ *      -64MB内存:地址0x80000000,长度0x4000000(其中0x83FFC000~0x84000000用于存储设备树)
+ *      -8250串口：地址0x10000000
+ *      -syscon:地址0x11100000,用于处理重启(0x7777)与关机(0x5555)
+ *      -clint: 地址0x11000000
+ */
+struct hminirv32ima_machine_default64mb
+{
+    hminirv32ima_core_t core;
+    uint8_t ram[0x3FFC000];
+    uint8_t dtb[0x4000000-0x3FFC000];
+    hs_common_serial_8250_t serial8250;
+    uint32_t syscon;
+    /** \brief 控制台是否有数据，此项由用户设置
+     *
+     * \param *machine const structhminirv32ima_machine_default64mb 机器指针
+     * \return bool 是否有数据
+     *
+     */
+    bool (*console_has_data)(const struct hminirv32ima_machine_default64mb *machine);
+    /** \brief 控制台输出数据，此项由用户设置
+     *
+     * \param *machine const structhminirv32ima_machine_default64mb 机器指针
+     * \param data uint8_t 数据
+     *
+     */
+    void (*console_put_data)(const struct hminirv32ima_machine_default64mb *machine,uint8_t data);
+    /** \brief
+     *
+     * \param *machine const structhminirv32ima_machine_default64mb 机器指针
+     * \return uint8_t 数据
+     *
+     */
+    uint8_t (*console_get_data)(const struct hminirv32ima_machine_default64mb *machine);
+};
+
+typedef struct hminirv32ima_machine_default64mb hminirv32ima_machine_default64mb_t;
+
+
+/** \brief 初始化默认机器
+ *
+ * \param machine hminirv32ima_machine_default64mb_t* 机器指针
+ *
+ */
+void hminirv32ima_machine_default64mb_init(hminirv32ima_machine_default64mb_t *machine);
+
+/** \brief 默认机器复位，与初始化的区别为不会清除内存中的数据
+ *
+ * \param machine hminirv32ima_machine_default64mb_t* 机器指针
+ *
+ */
+void hminirv32ima_machine_default64mb_reset(hminirv32ima_machine_default64mb_t *machine);
+
+/** \brief 默认机器加载镜像，注意：需要在初始化后使用
+ *
+ * \param machine hminirv32ima_machine_default64mb_t* 机器指针
+ * \param image const uint8_t* 内存镜像地址
+ * \param image_size size_t 镜像大小
+ * \return bool 是否成功
+ *
+ */
+bool hminirv32ima_machine_default64mb_load_image(hminirv32ima_machine_default64mb_t *machine,const uint8_t *image,size_t image_size);
+
+/** \brief 默认机器加载dtb，注意：需要在复位后使用,默认情况下在复位时会加载一个默认dtb
+ *
+ * \param machine hminirv32ima_machine_default64mb_t* 机器指针
+ * \param image const uint8_t* dtb地址
+ * \param image_size size_t dtb大小
+ * \return bool 是否成功
+ *
+ */
+bool hminirv32ima_machine_default64mb_load_dtb(hminirv32ima_machine_default64mb_t *machine,const uint8_t *dtb,size_t dtb_size);
+
+/** \brief 默认机器步进
+ *
+ * \param machine hminirv32ima_machine_default64mb_t* 机器指针
+ * \param elapsedUs uint32_t 上一步到本步间隔的微秒数，用于维护RISCV内部的定时器
+ * \param insn_count int 执行指令的条数
+ * \return int 错误代码，0=正常运行，1=WFI指令,0x5555=已关机,0x7777=已重启（需要用户自行重新初始化）,-1=参数错误
+ *
+ */
+int hminirv32ima_machine_default64mb_step(hminirv32ima_machine_default64mb_t *machine,uint32_t elapsedUs, int insn_count );
 
 #ifdef __cplusplus
 }
