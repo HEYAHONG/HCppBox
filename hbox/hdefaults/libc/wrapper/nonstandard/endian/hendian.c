c7b13be88e440f23acc945b21fd1a4964a14962c
HEYAHONG <2229388563@qq.com>
2026-06-07 02:15:21 +0800

    [hdefaults] 整理代码

--- hbox/hdefaults/libc/wrapper/nonstandard/endian/hendian.c
+++ hbox/hdefaults/libc/wrapper/nonstandard/endian/hendian.c
@@ -17,7 +17,7 @@ static inline void  hlibc_endian_bswap(uint8_t *bytes,size_t len)
 {
     if(bytes!=NULL && len > 0)
     {
-        for(size_t i=0;i<len/2;i++)
+        for(size_t i=0; i<len/2; i++)
         {
             uint8_t tmp=bytes[i];
             bytes[i]=bytes[len-1-i];
