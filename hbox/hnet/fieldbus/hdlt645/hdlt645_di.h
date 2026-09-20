4f69e9c1a2ac035dde1351819da1a5df2d2cf483
HEYAHONG <2229388563@qq.com>
2026-09-20 10:55:48 +0800

    [hdlt645] 添加A.3 变量数据标识表

--- hbox/hnet/fieldbus/hdlt645/hdlt645_di.h
+++ hbox/hnet/fieldbus/hdlt645/hdlt645_di.h
@@ -23,6 +23,18 @@ typedef enum
     HDLT645_DI_DATA_DESC_TYPE_XXXXXX_XX_SIGNED,
     HDLT645_DI_DATA_DESC_TYPE_XX_XXXX_YYMMDDHHMM,
     HDLT645_DI_DATA_DESC_TYPE_XX_XXXX_YYMMDDHHMM_SIGNED,
+    HDLT645_DI_DATA_DESC_TYPE_XXX_X,
+    HDLT645_DI_DATA_DESC_TYPE_XXX_X_SIGNED,
+    HDLT645_DI_DATA_DESC_TYPE_XX_XX,
+    HDLT645_DI_DATA_DESC_TYPE_XX_XX_SIGNED,
+    HDLT645_DI_DATA_DESC_TYPE_X_XXX,
+    HDLT645_DI_DATA_DESC_TYPE_X_XXX_SIGNED,
+    HDLT645_DI_DATA_DESC_TYPE_XXX_XXX,
+    HDLT645_DI_DATA_DESC_TYPE_XXX_XXX_SIGNED,
+    HDLT645_DI_DATA_DESC_TYPE_XX_XXXX,
+    HDLT645_DI_DATA_DESC_TYPE_XX_XXXX_SIGNED,
+    HDLT645_DI_DATA_DESC_TYPE_XXXXXXXX,
+    HDLT645_DI_DATA_DESC_TYPE_XXXXXXXX_SIGNED,
 } hdlt645_di_data_desc_type_t;
 
 /*
@@ -67,6 +79,102 @@ typedef struct
     uint8_t HH;                                             /**< 时,数值 */
     uint8_t mm;                                             /**< 分,数值 */
 } hdlt645_di_data_desc_type_xx_xxxx_yymmddhhmm_signed_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XXX_X
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xxx_x_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XXX_X_SIGNED
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xxx_x_signed_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XX_XX
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xx_xx_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XX_XX_SIGNED
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xx_xx_signed_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_X_XXX
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_x_xxx_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_X_XXX_SIGNED
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_x_xxx_signed_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XXX_XXX
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xxx_xxx_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XXX_XXX_SIGNED
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xxx_xxx_signed_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XX_XXXX
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xx_xxxx_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XX_XXXX_SIGNED
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xx_xxxx_signed_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XXXXXXXX
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xxxxxxxx_t;
+
+/*
+ * HDLT645_DI_DATA_DESC_TYPE_XXXXXXXX_SIGNED
+ */
+typedef struct
+{
+    double data;
+} hdlt645_di_data_desc_type_xxxxxxxx_signed_t;
 
 struct hdlt645_di_data_desc;
 typedef struct hdlt645_di_data_desc hdlt645_di_data_desc_t;
